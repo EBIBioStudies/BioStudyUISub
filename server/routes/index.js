d58cecbd4533fa5ca4607670e239b5adcfa6009f
mdylag <miroslawq.dylag@gmail.com>
2016-03-15 17:28:50 +0000

    Cleanup

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,8 @@
 'use stricvt';
 var express = require('express');
 var router = express.Router();
-
-
-var home = require('./home');
-var auth = require('./auth');
-var submission = require('./submission');
-var files = require('./files');
-var config = require('config');
-var userdata = require('./userdata');
-//var biosd=config.get('biostd');
 var proxy= require('./proxy');
 module.exports = function (app) {
-        app.use('/', home);
-        app.use('/auth',auth);
-        app.use('/data', submission);
-        app.use('/files', files);
-        app.use('/userdata', userdata);
         app.use('/proxy/api', proxy);
 
 };
--- server/routes/proxy.js
+++ server/routes/proxy.js
@@ -1,10 +1,14 @@
 'use strict';
-var _ =           require('underscore'),
-  path =      require('path'),
-  express = require('express'),
-  router = express.Router(),
-  config = require('config');
+var _ =           require('underscore');
+var path =      require('path');
+var  express = require('express')
+var router = express.Router();
+var config = require('config');
 var list = require('../models/submissions.json');
+var multipart = require('connect-multiparty');
+
+var Busboy = require('busboy');
+
 //var biosd=config.get('biostd');
 var submission = require('../models/submission.json');
 
@@ -82,6 +86,16 @@ router.post('/auth/signout',function(req, res, next) {
   res.status(200).json({});
 });
 
+router.post('/auth/signin',function(req, res, next) {
+  console.log('signin');
+  res.status(200).json({
+    "status": "OK",
+    "sessid": "Kbec9358596a94ba3cf35930a56216b26542f764f", "username": "demo"
+  });
+
+});
+
+
 router.post('/auth/signup',function(req, res, next) {
   console.log('signout');
   res.status(200).json({});
@@ -95,6 +109,34 @@ router.get('/files/dir',function(req, res) {
 router.delete('/files/delete',function(req, res) {
   res.json({});
 });
+
+router.post('/files/fileUpload', function (req, res) {
+  // request.files will contain the uploaded file(s),
+  // keyed by the input name (in this case, "file")
+  // show the uploaded file name
+  console.log('Data');
+  var busboy = new Busboy({ headers: req.headers });
+  console.log('Req', req.headers);
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+    //start saving the data
+    file.on('data', function(data) {
+      //write data
+      console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+    });
+    file.on('end', function() {
+      //close file
+      console.log('File [' + fieldname + '] Finished');
+      files.addFile(filename);
+    });
+  });
+  busboy.on('finish', function() {
+    res.end('upload complete');
+  });
+  req.pipe(busboy);
+});
+
 
 
 
