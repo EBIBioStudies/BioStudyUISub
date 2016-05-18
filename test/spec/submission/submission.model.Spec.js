786e93de69b62a8b497b2a5c5e56015c2dbc2020
olgamelnichuk <olkin@ebi.ac.uk>
2016-05-18 10:23:14 +0100

    submission model unit test: this is how submission  import works now

--- test/spec/submission/submission.model.Spec.js
+++ test/spec/submission/submission.model.Spec.js
@@ -40,21 +40,45 @@ describe("SubmissionModel", function () {
         var files = subm.files.items;
         expect(files.length).toEqual(1);
         expect(files[0].path).toEqual("/file");
-        var fattrs = files[0].attributes.attributes;
-        expect(fattrs.length).toEqual(1);
-        expect(fattrs[0].name).toEqual("Description");
-        expect(fattrs[0].value).toEqual("File Description");
+        var fileAttrs = files[0].attributes.attributes;
+        expect(fileAttrs.length).toEqual(1);
+        expect(fileAttrs[0].name).toEqual("Description");
+        expect(fileAttrs[0].value).toEqual("File Description");
 
         var links = subm.links.items;
         expect(links.length).toEqual(1);
         expect(links[0].url).toEqual("http://example.com");
-        expect(links[0].attributes.attributes.length).toEqual(1); // 'Description' added automatically as it's required
-        var lattrs = links[0].attributes.attributes;
-        expect(lattrs.length).toEqual(1);
-        expect(lattrs[0].name).toEqual("Description");
-        expect(lattrs[0].value).toEqual("");
+        var linkAttrs = links[0].attributes.attributes;
+        expect(linkAttrs.length).toEqual(1); // 'Description' added automatically as it's required
+        expect(linkAttrs[0].name).toEqual("Description");
+        expect(linkAttrs[0].value).toEqual("");
+        
+        var publications = subm.publications.items;
+        expect(publications.length).toEqual(1);
+        var publAttrs = publications[0].attributes.attributes;
+        expect(publAttrs.length).toEqual(5);
+        expect(publAttrs[0].name).toEqual("Title");
+        expect(publAttrs[0].value).toEqual("Publication Title");
+        expect(publAttrs[1].name).toEqual("Journal");
+        expect(publAttrs[1].value).toEqual("PLoS biology");
+        expect(publAttrs[2].name).toEqual("Volume");
+        expect(publAttrs[2].value).toEqual("3(1)");
+        expect(publAttrs[3].name).toEqual("Pages");
+        expect(publAttrs[3].value).toEqual("e15");
+        expect(publAttrs[4].name).toEqual("Publication date");
+        expect(publAttrs[4].value).toEqual("2005 Jan");
 
-
-
+        var contacts = subm.contacts.items;
+        expect(contacts.length).toEqual(1);
+        var contactAttrs = contacts[0].attributes.attributes;
+        expect(contactAttrs.length).toEqual(4); // 'Name', 'Organisation', 'E-mqil' added automatically as they're required
+        expect(contactAttrs[0].name).toEqual("Name");
+        expect(contactAttrs[0].value).toEqual("John Doe");
+        expect(contactAttrs[1].name).toEqual("Organisation");
+        expect(contactAttrs[1].value).toEqual("");
+        expect(contactAttrs[2].name).toEqual("E-mail");
+        expect(contactAttrs[2].value).toEqual("");
+        expect(contactAttrs[3].name).toEqual("Affiliation");
+        expect(contactAttrs[3].value).toEqual("affilRef1");
     });
 });
