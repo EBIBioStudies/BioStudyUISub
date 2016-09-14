079a0bc9d22996bad83697c03d35d83e972489f8
olgamelnichuk <olkin@ebi.ac.uk>
2016-09-14 12:43:16 +0100

    added thor integration on signup form

--- app/lib/auth/pages/signup/signup.ctrl.js
+++ app/lib/auth/pages/signup/signup.ctrl.js
@@ -1,5 +1,5 @@
 export default class SignUpController {
-    constructor($scope, $log, AuthService, vcRecaptchaService) {
+    constructor($scope, $timeout, $document, $log, AuthService, vcRecaptchaService) {
         'ngInject';
 
         $scope.user = {};
@@ -23,5 +23,29 @@ export default class SignUpController {
         $scope.resetError = function () {
             $scope.error = null;
         };
+
+        $scope.findOrcid = function () {
+            $log.debug("findOrcid");
+            var thorIFrame = $document[0].getElementById("thor");
+            $log.debug(thorIFrame);
+
+            var w = thorIFrame.contentWindow;
+            $log.debug(w);
+
+            var unbind = w.addCallback(function(msg) {
+                var data = angular.fromJson(msg);
+                var orcid = data['orcid-profile']['orcid-identifier']['path'];
+                $log.debug(orcid);
+
+                /* workaround to update model and form values ASAP */
+                var promise = $timeout(function() {
+                    $log.debug("on timeout: user.orcid=" + orcid);
+                    $scope.user.orcid = orcid;
+                    $timeout.cancel(promise);
+                }, 1);
+                unbind();
+            });
+            w.openPopup();
+        }
     }
 }
