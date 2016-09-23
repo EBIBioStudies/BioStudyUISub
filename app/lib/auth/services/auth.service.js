e0fcc1824ef71fd4edf4be9d719b29d5ebf55d37
olgamelnichuk <olkin@ebi.ac.uk>
2016-09-23 10:49:36 +0100

    bugfix: $q.defer is not needed..

--- app/lib/auth/services/auth.service.js
+++ app/lib/auth/services/auth.service.js
@@ -10,7 +10,6 @@ export default class AuthService {
 
         Object.assign(this, {
             signIn(credentials) {
-                var defer = $q.defer();
                 $http.post("/raw/auth/signin", credentials)
                     .then(
                         function (response) {
@@ -18,106 +17,95 @@ export default class AuthService {
                             if (data.status === "OK") {
                                 Session.create(data.sessid, data.username, data.email || "", USER_ROLES.user);
                             }
-                            defer.resolve(data);
+                            return data;
                         },
                         function (response) {
                             if (response.status === 403) {
                                 response.data.message = "Invalid credentials";
-                                defer.resolve(response.data);
-                                return;
+                                return response.data;
                             }
                             $log.error("login error", response);
-                            defer.reject(response);
+                            return $q.reject(response);
                         });
-                return defer.promise;
             },
 
             signOut() {
                 if (!this.isAuthenticated()) {
                     return $q.when({});
                 }
-                var defer = $q.defer();
-                $http.post("/api/auth/signout", {username: Session.userName})
+                return $http.post("/api/auth/signout", {username: Session.userName})
                     .then(
                         function () {
                             Session.destroy();
-                            defer.resolve({});
+                            return {};
                         },
                         function (response) {
                             $log.error("logout error", response);
-                            defer.reject(response);
+                            return $q.reject(response);
                         });
-                return defer.promise;
             },
 
             signUp(user) {
-                var defer = $q.defer();
                 user.path = getAppPath() + "#/activate";
-                $http.post("/api/auth/signup", user)
+                return $http.post("/api/auth/signup", user)
                     .then(
                         function (response) {
-                            defer.resolve(response.data);
+                            return response.data;
                         },
                         function (response) {
                             if (response.status === 403 || response.status === 400) {
-                                defer.resolve(response.data);
-                                return;
+                                return response.data;
                             }
                             $log.error("signup error", response);
-                            defer.reject(response);
+                            return $q.reject(response);
                         });
-                return defer.promise;
             },
 
             activate(key) {
-                var defer = $q.defer();
-                $http.post("/raw/auth/activate/" + key)
+                return $http.post("/raw/auth/activate/" + key)
                     .then(
                         function (response) {
-                            defer.resolve(response.data);
+                            return response.data;
                         },
                         function (response) {
                             $log.error("activate error", response);
-                            defer.reject(response);
+                            return $q.reject(response);
                         });
-                return defer.promise;
             },
 
             passwordResetRequest(email, recaptcha) {
-                var defer = $q.defer();
                 var path = getAppPath() + "#/password_reset";
-                $http.post("/api/auth/passrstreq/", {email: email, path: path, 'recaptcha2-response': recaptcha})
+                return $http.post("/api/auth/passrstreq/", {email: email, path: path, 'recaptcha2-response': recaptcha})
                     .then(
                         function (response) {
                             defer.resolve(response.data);
                         },
                         function (response) {
                             if (response.status === 403) {
-                                defer.resolve(response.data);
-                                return;
+                                return response.data;
                             }
                             $log.error("password reset request error", response);
-                            defer.reject(response);
+                            return $q.reject(response);
                         });
-                return defer.promise;
             },
 
             passwordReset(key, password, recaptcha) {
-                var defer = $q.defer();
-                $http.post("/raw/auth/passreset/", {key: key, password: password, 'recaptcha2-response': recaptcha})
+                return $http.post("/raw/auth/passreset/", {
+                    key: key,
+                    password: password,
+                    'recaptcha2-response': recaptcha
+                })
                     .then(
                         function (response) {
-                            defer.resolve(response.data);
+                            return response.data;
                         },
                         function (response) {
                             if (response.status === 400) { //invalid request
-                                defer.resolve(response.data);
-                                return;
+                                return response.data;
                             }
                             $log.error("password reset error", response);
-                            defer.reject(response);
+                            return q$.reject(response);
                         });
-                return defer.promise;
             },
 
             isAuthenticated() {
