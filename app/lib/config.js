50bf2b356f391938be7711d046162c41d08e0385
olgamelnichuk <olkin@ebi.ac.uk>
2016-10-03 14:53:12 +0100

    version 1.0.2

--- app/lib/config.js
+++ app/lib/config.js
@@ -2,4 +2,4 @@ import angular from 'angular';
 export default angular.module("BioStudyApp.config", [])
 .constant("APP_PROXY_BASE", "/proxy")
 .constant("APP_DEBUG_ENABLED", true)
-.constant("APP_VERSION", "1.0.1");
+.constant("APP_VERSION", "1.0.2");
