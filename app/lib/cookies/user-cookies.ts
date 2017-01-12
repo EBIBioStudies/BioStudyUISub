0818580cc193e1dcfefd7b53c8f8402d37d4663b
olgamelnichuk <olkin@ebi.ac.uk>
2017-01-12 13:59:39 +0000

    bugfix: use cookie path when deleting them

--- app/lib/cookies/user-cookies.ts
+++ app/lib/cookies/user-cookies.ts
@@ -2,10 +2,11 @@ import {Cookie} from 'ng2-cookies/ng2-cookies';
 
 const AE_LOGGED_USER = "AeLoggedUser_";
 const AE_LOGIN_TOKEN = "AeLoginToken_";
+const PATH = '/';
 
 export function setUserCookies(login, token): void {
-    Cookie.set(AE_LOGGED_USER, login, 30, '/');
-    Cookie.set(AE_LOGIN_TOKEN, token, 30, '/');
+    Cookie.set(AE_LOGGED_USER, login, 30, PATH);
+    Cookie.set(AE_LOGIN_TOKEN, token, 30, PATH);
 }
 
 export function getUserCookies(): any {
@@ -16,6 +17,6 @@ export function getUserCookies(): any {
 }
 
 export function destroyUserCookies(): void {
-    Cookie.delete(AE_LOGGED_USER);
-    Cookie.delete(AE_LOGIN_TOKEN);
+    Cookie.delete(AE_LOGGED_USER, PATH);
+    Cookie.delete(AE_LOGIN_TOKEN, PATH);
 }
