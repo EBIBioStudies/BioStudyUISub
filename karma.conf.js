54e19b797b58d8c829eec6a755b8e8d7f0b84b70
Olga Melnichuk <olga.melnichuk@gmail.com>
2017-05-11 13:36:23 +0100

    rename app -> src

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     var configuration = {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: './app',
+        basePath: 'src',
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
