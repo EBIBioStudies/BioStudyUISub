73e55fb4d0930fca0b00ac376695f64ae03c38cc
Hector Casanova <hector@hector-md.windows.ebi.ac.uk>
2017-08-24 17:17:50 +0100

    Improves separation of concerns between submission items and sidebar component

--- src/app/submission/edit/subm-sidebar/subm-sidebar.component.ts
+++ src/app/submission/edit/subm-sidebar/subm-sidebar.component.ts
@@ -21,6 +21,81 @@ import {ConfirmDialogComponent} from 'app/shared/index';
 import {SubmAddEvent} from '../subm-add/subm-add-event.model';
 import {SectionType} from '../../shared/submission-type.model';
 
+class SubmItem {
+    feature: Feature;
+    icon: string;
+    private _deleted: boolean;
+
+    constructor(feature: Feature, icon: string = 'fa-file-o') {
+        this.feature = feature;
+        this.icon = icon;
+        this._deleted = false;
+    }
+
+    get isDeleted(): boolean {
+        return this._deleted;
+    }
+
+    delete(): void {
+        this._deleted = true;
+    }
+
+    reset(): void {
+        this._deleted = false;
+    }
+
+    isReadOnly(): boolean {
+        return !this.feature.type.canModify;
+    }
+
+    onClick(event: Event): void {
+        if (this.feature.singleRow) {
+            this.feature.addColumn();
+            return;
+        }
+
+        if (this.feature.colSize() === 0) {
+            this.feature.addColumn();
+        }
+
+        this.feature.addRow();
+    }
+}
+
+class SubmItems {
+    private _isDeletion: boolean;    //has any of the items been deleted?
+    private _items: SubmItem[];
+
+    constructor(section: Section) {
+        this._items = [];
+        this._isDeletion = false;
+
+        this._items.push(new SubmItem(section.annotations));
+        section.features.list().forEach(
+            feature => this._items.push(new SubmItem(feature))
+        );
+    }
+
+    get isDeletion(): boolean {
+        return this._isDeletion;
+    }
+
+    list(): SubmItem[] {
+        return this._items;
+    }
+
+    delete(itemIdx: number): void {
+        this._items[itemIdx].delete();
+        this._isDeletion = true;
+    }
+
+    reset(): void {
+        this._items.forEach(item => item.reset());
+        this._isDeletion = false;
+    }
+}
+
+//TODO: Use model-driven approach to make dynamic manipulation of form more syntactic
 @Component({
     selector: 'subm-sidebar',
     templateUrl: './subm-sidebar.component.html',
@@ -36,8 +111,8 @@ export class SubmSideBarComponent implements OnChanges {
     @ViewChild('confirmDialog')
     confirmDialog: ConfirmDialogComponent;
 
-    editing: boolean = false;   //component's mode: display or editing, with different renderings
-    items: any[] = [];          //items collection
+    editing: boolean = false;           //component's mode: display or editing, with different renderings
+    items: SubmItems;                   //current collection of feature/subsection items
 
     private subscr: Subscription;
 
@@ -62,10 +137,6 @@ export class SubmSideBarComponent implements OnChanges {
         }
     }
 
-    get sectionType(): SectionType {
-        return this.section.type;
-    }
-
     onToggle(ev): void {
         ev.preventDefault();
         if (this.toggle) {
@@ -82,14 +153,23 @@ export class SubmSideBarComponent implements OnChanges {
 
         if (form.valid) {
 
-            //Field names suffixed with the corresponding items array index
-            Object.keys(form.value).forEach(key => {
-                const itemsIdx = key.split('_')[1];
+            //Updates items collection if there has been scalar changes
+            if (form.dirty) {
+                Object.keys(form.value).forEach(key => {
+                    const itemsIdx = key.split('_')[1];     //Field names suffixed with items array index
 
-                items[itemsIdx].feature.typeName = form.value[key];
-            });
+                    items[itemsIdx].feature.typeName = form.value[key];
+                });
+            }
         }
         this.onEditModeToggle();
+    }
+
+    onCancel(event: Event): void {
+        if (this.items.isDeletion) {
+            this.items.reset();
+        }
+        this.onEditModeToggle(event);
     }
 
     /**
@@ -107,56 +187,31 @@ export class SubmSideBarComponent implements OnChanges {
     }
 
     onAdd(ev: SubmAddEvent) {
+        const rootType: SectionType = this.section.type;
+
         if (ev.itemType === 'Section') {
-            const sectionType = this.sectionType.getSectionType(ev.name);
+            const sectionType = rootType.getSectionType(ev.name);
             this.section.sections.add(sectionType);
             return;
         }
         const idx = ['AttributeList', 'AttributeGrid'].indexOf(ev.itemType);
         if (idx > -1) {
             const singleRow = idx === 0;
-            const featureType = this.sectionType.getFeatureType(ev.name, singleRow);
+            const featureType = rootType.getFeatureType(ev.name, singleRow);
             const f = this.section.features.add(featureType);
         }
     }
 
-    onItemDelete(itemIdx: number): void {
-        this.confirm().subscribe(() => {
-            this.removeItemAt(itemIdx);
-        });
+    onItemDelete(event: Event, itemIdx: number): void {
+        event.preventDefault();
+        this.items.delete(itemIdx);
     }
 
     onItemsChange(): void {
-        const items = [];
-        items.push(this.createItem(this.section.annotations));
-        this.section.features.list().forEach(
-            (f: Feature) => items.push(this.createItem(f))
-        );
-        this.items = items;
+        this.items = new SubmItems(this.section);
     }
 
     private confirm(): Observable<any> {
         return this.confirmDialog.confirm();
-    }
-
-    private createItem(f: Feature): any {
-        return {
-            feature: f,
-            icon: 'fa-file-o',
-            onClick: (ev) => {
-                if (f.singleRow) {
-                    f.addColumn();
-                    return;
-                }
-                if (f.colSize() === 0) {
-                    f.addColumn();
-                }
-                f.addRow();
-            }
-        };
-    }
-
-    private removeItemAt(index: number): void {
-        this.items.splice(index, 1);
     }
 }
