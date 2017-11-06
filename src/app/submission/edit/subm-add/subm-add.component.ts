7b4bd469c4377e9e4b8cf54cb44d2191cd5cf7c4
Hector Casanova <hector@hector-md.windows.ebi.ac.uk>
2017-11-06 10:43:16 +0000

    Makes type add form's status consistent with actions in subm-add modal

--- src/app/submission/edit/subm-add/subm-add.component.ts
+++ src/app/submission/edit/subm-add/subm-add.component.ts
@@ -7,18 +7,21 @@ import {
 } from '@angular/core';
 
 import {ModalDirective} from 'ngx-bootstrap';
-import {AbstractControl, NgForm, NgModel} from "@angular/forms";
+import {
+    NgForm,
+    NgModel
+} from "@angular/forms";
 
 @Component({
     selector: 'subm-add-dialog',
     templateUrl: './subm-add.component.html'
 })
 export class SubmAddDialogComponent implements AfterViewInit {
-    _type: string = 'Grid';      //form model member for the type property
+    private _type: string;       //form model member for the type property
     name: string;                //form model member for the name property
-    isUnique: boolean = true;    //flag for the uniqueness of the chosen type name
+    isUnique: boolean;           //uniqueness flag of the chosen type name
 
-    @Input() addType: Function = () => {};
+    @Input() onAddType: Function = () => {}; //callback for add action (acts as an ad-hoc, async validator)
 
     @ViewChild('focusBtn')
     private focusEl: ElementRef;
@@ -42,16 +45,26 @@ export class SubmAddDialogComponent implements AfterViewInit {
         this._type = value;
     }
 
+    ngOnInit(): void {
+        this.type = 'Grid';
+    }
+
+    /**
+     * Renders the modal with default values.
+     */
     show(): void {
         this.modalDirective.show();
     }
 
+    /**
+     * Closes the modal.
+     */
     hide(): void {
         this.modalDirective.hide();
     }
 
     /**
-     * Every time the type name is blank, it will be marked as invalid (no existing type name is blank after all).
+     * Every time the type name is blank, it will be marked as unique (no existing type name is blank after all).
      */
     ngAfterViewInit() {
         this.typeName.statusChanges.subscribe((status) => {
@@ -68,6 +81,21 @@ export class SubmAddDialogComponent implements AfterViewInit {
         this.focusEl.nativeElement.focus();
     }
 
+    /**
+     * Handler for click events on "Cancel" button. Resets the form's state and hides modal.
+     */
+    onCancel(): void {
+        this.type = 'Grid';
+        this.typeName.control.reset();
+        this.hide();
+    }
+
+    /**
+     * Shows validation errors globally on submission according to the nature of the type, using the addition operation
+     * as a validator post-submission. Only after that op is the form data confirmed as valid.
+     * modal is simply
+     * @param {NgForm} form
+     */
     onSubmit(form: NgForm): void {
         let isSingleRow: boolean = false;
         let isSection: boolean = false;
@@ -81,11 +109,9 @@ export class SubmAddDialogComponent implements AfterViewInit {
             }
 
             //Add operation successful => gets form ready for further additions
-            if (this.addType(this.name, isSection, isSingleRow)) {
+            if (this.onAddType(this.name, isSection, isSingleRow)) {
                 this.hide();
-                form.reset();
-                this.isUnique = true;
-                this.type = 'Grid';
+                form.reset({type: 'Grid', name: ''});
 
             //Add operation failed => type name was not unique => show error
             } else {
