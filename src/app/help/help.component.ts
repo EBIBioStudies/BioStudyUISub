1ba584492c62eabec66838150e59e35e2563877d
Hector Casanova <hector@hector-md.windows.ebi.ac.uk>
2018-02-26 10:54:40 +0000

    Further improvements to the help section

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,8 +1,29 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'help-page',
     templateUrl: './help.component.html',
     styleUrls: ['/help.component.css']
 })
-export class HelpComponent {}
+
+/**
+ * Makes in-page navigation possible. Angular's routing tends to take over even if the target
+ * is just a fragment.
+ */
+export class HelpComponent {
+    jumpTo(event): void {
+
+        //Cancels any routing
+        event.preventDefault();
+
+        //Scrolls down to the relevant section taking into account sticky header's height.
+        try {
+            const sectionEl = document.querySelector(event.target.getAttribute('href'));
+            window.scrollBy(0, sectionEl.getBoundingClientRect().top - 100);
+
+        } catch (exception) {
+            console.error('Target unknown');
+        }
+    }
+}
