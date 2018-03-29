fff4c176c0504a6eceb7944e221ad354a2c4265c
Hector Casanova <hector@hector-md.windows.ebi.ac.uk>
2018-03-29 13:45:35 +0100

    Adjusts overlay to alert's size, simplifies logic and adds slide-in animation

--- src/app/core/header/global-error.component.ts
+++ src/app/core/header/global-error.component.ts
@@ -1,39 +1,45 @@
 import {
     ErrorHandler,
     NgZone,
-    Component
+    Component, ElementRef
 } from '@angular/core';
 
 import {GlobalErrorHandler} from 'app/global-error.handler';
 
 @Component({
     selector: 'global-error',
-    template: `
-        <div *ngFor="let error of errors" style="position:absolute;width:100%;z-index:1000">
-            <alert type="danger" dismissible="true">
-                {{message(error)}}
-            </alert>
-        </div>
-    `
+    templateUrl: './global-error.component.html',
+    styleUrls: ['./global-error.component.css']
 })
-
 export class GlobalErrorComponent {
-    errors = [];
+    error = '';
 
-    constructor(geh: ErrorHandler,
-                zone: NgZone) {
+    /**
+     * Captures async errors and refreshes the UI (shows an alert) in sync with Angular's change detection mechanism.
+     * @param {ErrorHandler} geh - Global handler for errors.
+     * @param {NgZone} zone - Global forked zone.
+     */
+    constructor (geh: ErrorHandler, zone: NgZone, rootEl: ElementRef) {
         if (geh instanceof GlobalErrorHandler) {
             geh.anErrorDetected$.subscribe(error => {
                 zone.run(() => {
-                    this.errors = [error];
+                    this.error = this.message(error);
+                    setTimeout(() => {
+                        rootEl.nativeElement.firstChild.classList.add('slide-in');
+                    });
                 });
             });
         }
     }
 
+    /**
+     * Merges different error properties to produce the string to be shown as an error message.
+     * @param error - Error object containing fragments of the error message.
+     * @returns {string} - Text to be used as an error message.
+     */
     message(error: any): string {
         const name = error.name || '';
         const message = error.message || '';
-        return (name + message).length === 0 ? 'Oops! Something went wrong..' : (name + ' ' + message);
+        return (name + message).length === 0 ? 'Unknown error' : (name + ' ' + message);
     }
 }
