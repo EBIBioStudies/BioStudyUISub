f5a0c312e413c92b7073126562555099d63ad61e
olgamelnichuk <olkin@ebi.ac.uk>
2018-08-16 12:23:08 +0100

    fixes the path for the user local files; removes '/' in the beginning

--- src/app/submission-shared/file-select/file-tree.store.ts
+++ src/app/submission-shared/file-select/file-tree.store.ts
@@ -74,8 +74,9 @@ export class FileTreeStore {
         return this.getUserGroups()
             .map(groups => groups.find(g => g.id !== undefined && fileDir.startsWith(g.id)))
             .flatMap(group => group ?
-                Observable.of(fileDir.replace(group.id, '/Groups/' + group.name + '/')) :
+                Observable.of(fileDir.replace(group.id, '/Groups/' + group.name)) :
                 Observable.of(fileDir))
+            .map(dir => dir === '' ? dir : dir + '/')
             .map(dir => dir + fileName)
         /*.flatMap(dir => this.getFiles(dir))
         .catch((err) => {
