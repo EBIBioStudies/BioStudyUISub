84d0c092595bc95fc75d6fcca1edf4387558a05a
olgamelnichuk <olkin@ebi.ac.uk>
2018-09-14 12:48:43 +0100

    removes merge as it's not needed any more

--- src/app/file/file-list/file-upload-badge/file-upload-badge.component.ts
+++ src/app/file/file-list/file-upload-badge/file-upload-badge.component.ts
@@ -20,22 +20,21 @@ export class FileUploadBadgeComponent implements OnDestroy {
 
     private sb?: Subscription;
     private uploadItems: UploadBadgeItem[] = [];
-    hasFailedUploads: boolean = false;       //has any upload request failed?
+    hasFailedUploads: boolean = false;
 
     constructor(fileUploadList: FileUploadList) {
         this.sb = fileUploadList.activeUploadsChanged$.subscribe(activeUploads => {
 
             this.hasFailedUploads = activeUploads.find(req => req.isFailed()) !== undefined;
 
-            //Collection of upload operations, one per file (as opposed to multiple-file requests).
-            return this.merge(
+            this.uploadItems =
                 activeUploads.map(fileUpload =>
                     fileUpload.fileNames.map(fileName => ({
                         fileName: fileName,
                         filePath: fileUpload.filePath,
                         progress: fileUpload.isFailed() ? 'error' : fileUpload.progress + '%'
                     }))
-                ).reduce((rv, v) => rv.concat(v), (<UploadBadgeItem[]>[])));
+                ).reduce((rv, v) => rv.concat(v), (<UploadBadgeItem[]>[]));
         })
     }
 
@@ -45,38 +44,6 @@ export class FileUploadBadgeComponent implements OnDestroy {
 
     get uploads(): UploadBadgeItem[] {
         return this.uploadItems;
-    }
-
-    merge(dest: UploadBadgeItem[]): UploadBadgeItem[] {
-        console.log('merge', dest)
-        if (this.uploadItems.length == 0) {
-            this.uploadItems = dest;
-            return dest;
-        }
-        let src = this.uploadItems;
-        let i = 0, j = 0;
-        while (i < src.length || j < dest.length) {
-            if (i >= src.length && j < dest.length) {
-                src.push(dest[j]);
-                continue;
-            }
-            if (j >= dest.length && i < src.length) {
-                src.splice(i, 1);
-                continue;
-            }
-            let u1 = src[i];
-            let u2 = dest[j];
-            if (u1.fileName != u2.fileName) {
-                src.splice(i, 1);
-                i++;
-                continue;
-            }
-            u1.progress = u2.progress;
-            i++;
-            j++;
-        }
-        console.log(this.uploadItems.length);
-        return src;
     }
 
     onMenuItemClick(uploadItem: UploadBadgeItem) {
