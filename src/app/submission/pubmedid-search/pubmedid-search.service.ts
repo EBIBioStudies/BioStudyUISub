f49ecc6d2385bce4e7869b4dccafba3b6de6b7f6
olgamelnichuk <olkin@ebi.ac.uk>
2019-02-20 15:55:57 +0000

    updates europepmc id search service to use original service url (not via proxy)

--- src/app/submission/pubmedid-search/pubmedid-search.service.ts
+++ src/app/submission/pubmedid-search/pubmedid-search.service.ts
@@ -1,7 +1,17 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable, of, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
+
+const PublAttrMapping = {
+    title: 'title',
+    authorString: 'authors',
+    pubType: 'type',
+    issue: 'issue',
+    journalIssn: 'issn',
+    pubYear: 'year',
+    journalVolume: 'volume'
+};
 
 @Injectable()
 export class PubMedSearchService {
@@ -13,7 +23,20 @@ export class PubMedSearchService {
             console.warn('PubMedSearch: no pubMedId given');
             return of({});
         }
-        return this.http.get('/api/pubMedSearch/' + pmid).pipe(
+        return this.http.get<any>(`https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ext_id:${pmid}&format=json`).pipe(
+            map(resp => {
+                let hitCount = resp.hitCount;
+                let data = {};
+                if (hitCount >= 1) {
+                    let publ = resp.resultList.result[0];
+                    Object.keys(PublAttrMapping).forEach(key => {
+                        if (publ.hasOwnProperty(key)) {
+                            data[PublAttrMapping[key]] = publ[key] + '';
+                        }
+                    });
+                }
+                return data;
+            }),
             catchError((error: any) => {
                 let err = {status: '', message: ''};
                 try {
