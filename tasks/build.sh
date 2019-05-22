23cfc1e887cb484cb642abb85becf2b4e41fc284
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-22 12:00:08 +0100

    - Build and generate compressed artifact

---
+++ tasks/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Print shell input lines as they are read
+set -v
+
+# Clean build dist folder
+rm -rf dist && mkdir dist
+
+# Build assets into dist/public folder
+npx ng build --outputPath=dist --deleteOutputPath=true
+
+# Create artifact
+tar -czf ~/subtool.tar.gz -C dist .
