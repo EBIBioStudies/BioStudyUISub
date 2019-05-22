6c9ad43e4e8fd9f25833f0a7a0aeec1c5598ac8a
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-22 15:23:15 +0100

    - Include build_proxy bash script

---
+++ tasks/build_proxy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Clone Proxy into proxy folder
+git clone https://github.com/EBIBioStudies/BioStudySubmProxy.git proxy
+
+# Go to proxy folder
+cd proxy
+
+# Get values for properties from Environment Variables
+bsBackendUrl=${PROXY_BACKEND_URL}
+bsBackendUrl_4tests=${PROXY_BACKEND_URL_TESTS}
+logsDir=${CATALINA_BASE}/logs/ves-hx-74
+logLevel=${PROXY_LOG_LEVEL}
+httpsFilterDisabled=${PROXY_HTTPS_FILTER_DISABLED}
+offlineMode=${PROXY_OFFLINE_MODE}
+offlineUserDir=${PROXY_OFFLINE_MODE_DIR}
+
+# Copy properties to gradle.properties
+echo -e "
+bsBackendUrl=${bsBackendUrl}
+bsBackendUrl_4tests=${bsBackendUrl_4tests}
+logsDir=${logsDir}
+logLevel=${logLevel}
+httpsFilterDisabled=${httpsFilterDisabled}
+offlineMode=${offlineMode}
+offlineUserDir=${offlineUserDir}
+" > gradle.properties
+
+gradle clean build war -x test
