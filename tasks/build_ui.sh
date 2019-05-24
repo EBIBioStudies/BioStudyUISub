6f60f1740a6343658ff0d6d1acf09dc5be6cb819
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-24 15:33:47 +0100

    - Env variables to UI config

--- tasks/build_ui.sh
+++ tasks/build_ui.sh
@@ -6,6 +6,19 @@ set -v
 # Clean build dist folder
 rm -rf dist && mkdir dist
 
+# Config properties
+appProxyBase=$APP_PROXY_BASE;
+appContext=$APP_CONTEXT;
+appDebugEnabled=$APP_DEBUG_ENABLED;
+appInstanceKey=$APP_INSTANCE_KEY;
+
+# Update config
+sed -i 's/\(.*"APP_PROXY_BASE":\)\(.*\)/\1"'$appProxyBase'",/' config.json
+sed -i 's/\(.*"APP_CONTEXT":\)\(.*\)/\1"'$appContext'",/' config.json
+sed -i 's/\(.*"APP_DEBUG_ENABLED":\)\(.*\)/\1'$appDebugEnabled',/' config.json
+# Last line, don't append a comma
+sed -i 's/\(.*"APP_INSTANCE_KEY":\)\(.*\)/\1"'$appInstanceKey'"/' config.json
+
 # Build assets into dist/public folder
 npx ng build --outputPath=dist --deleteOutputPath=true
 
