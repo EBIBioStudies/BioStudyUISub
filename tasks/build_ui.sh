f769cf21f23879140e9887b3093e794ea6db165d
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-24 15:47:30 +0100

    - Test sed

--- tasks/build_ui.sh
+++ tasks/build_ui.sh
@@ -13,11 +13,11 @@ appDebugEnabled=$APP_DEBUG_ENABLED;
 appInstanceKey=$APP_INSTANCE_KEY;
 
 # Update config
-sed -i 's/\(.*"APP_PROXY_BASE":\)\(.*\)/\1"'$appProxyBase'",/' config.json
-sed -i 's/\(.*"APP_CONTEXT":\)\(.*\)/\1"'$appContext'",/' config.json
-sed -i 's/\(.*"APP_DEBUG_ENABLED":\)\(.*\)/\1'$appDebugEnabled',/' config.json
+sed -i 's/"APP_PROXY_BASE":(.*)/"'$appProxyBase'",' config.json
+# sed -i 's/\(.*"APP_CONTEXT":\)\(.*\)/\1"'$appContext'",/' config.json
+# sed -i 's/\(.*"APP_DEBUG_ENABLED":\)\(.*\)/\1'$appDebugEnabled',/' config.json
 # Last line, don't append a comma
-sed -i 's/\(.*"APP_INSTANCE_KEY":\)\(.*\)/\1"'$appInstanceKey'"/' config.json
+# sed -i 's/\(.*"APP_INSTANCE_KEY":\)\(.*\)/\1"'$appInstanceKey'"/' config.json
 
 # Build assets into dist/public folder
 npx ng build --outputPath=dist --deleteOutputPath=true
