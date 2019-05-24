6daf70355cf1294e96d30382e93a652e34a99e19
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-24 16:42:53 +0100

    - Write env variable values into config.json

--- tasks/build_ui.sh
+++ tasks/build_ui.sh
@@ -12,12 +12,12 @@ appContext=$APP_CONTEXT;
 appDebugEnabled=$APP_DEBUG_ENABLED;
 appInstanceKey=$APP_INSTANCE_KEY;
 
-# Update config
+# Update config.json
 sed -i 's%"APP_PROXY_BASE":.*%"APP_PROXY_BASE":"'$appProxyBase'",%' src/config.json
-# sed -i 's/\(.*"APP_CONTEXT":\)\(.*\)/\1"'$appContext'",/' config.json
-# sed -i 's/\(.*"APP_DEBUG_ENABLED":\)\(.*\)/\1'$appDebugEnabled',/' config.json
+sed -i 's%"APP_CONTEXT":.*%"APP_CONTEXT":"'$appContext'",%' src/config.json
+sed -i 's%"APP_DEBUG_ENABLED":.*%"APP_DEBUG_ENABLED":'$appDebugEnabled',%' src/config.json
 # Last line, don't append a comma
-# sed -i 's/\(.*"APP_INSTANCE_KEY":\)\(.*\)/\1"'$appInstanceKey'"/' config.json
+sed -i 's%"APP_INSTANCE_KEY".*%"APP_INSTANCE_KEY":"'$appInstanceKey'"%' src/config.json
 
 # Build assets into dist/public folder
 npx ng build --outputPath=dist --deleteOutputPath=true
