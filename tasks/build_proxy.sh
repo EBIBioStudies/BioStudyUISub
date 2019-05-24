113e2b2264f8409dd7c94537a02cb0074030b4d2
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-05-24 11:04:42 +0100

    - Copy artifacts in proxy/WebContent

--- tasks/build_proxy.sh
+++ tasks/build_proxy.sh
@@ -8,7 +8,7 @@ git clone https://github.com/EBIBioStudies/BioStudySubmProxy.git proxy
 
 # Copy the artifacts generated in build_ui stage
 if [ -f subtool.tar.gz ]; then
-  tar -xvf subtool.tar.gz -C proxy/src/main/webapp;
+  tar -xvf subtool.tar.gz -C proxy/WebContent
 fi
 
 # Get property values from Environment Variables
