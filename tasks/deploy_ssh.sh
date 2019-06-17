1702199e41ef86ad51913c8a0aeffe37121438f0
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-06-17 10:15:46 +0100

    - Testing SSH key

--- tasks/deploy_ssh.sh
+++ tasks/deploy_ssh.sh
@@ -9,13 +9,11 @@ mkdir -p ~/.ssh
 # Copy the SSH key stored in SSH_KEY env var into id_rsa
 echo "$SSH_KEY" | tr -d '\r' > ~/.ssh/id_rsa
 
-echo $SSH_KEY
-
 # Give the right permissions
-# chmod 700 ~/.ssh/id_rsa
+chmod 700 ~/.ssh/id_rsa
 
 # Run ssh-agent (inside the build environment)
-# eval $(ssh-agent -s)
+eval $(ssh-agent -s)
 
 # Add the SSH key stored in SSH_KEY variable to the agent store
-# ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
