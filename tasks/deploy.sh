a781d26fefca9e32f4daff55cbe0ae99bb4a072b
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-06-18 11:12:09 +0100

    - Fix host key checking

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -10,7 +10,7 @@ artifact=proxy-${CI_ENVIRONMENT_SLUG}.war
 restart_command="cd ${VM_HOME_DIR} && bin/stop && bin/clean_dirs && cp deploy/$artifact webapps/ && bin/start"
 
 # Copy Artifacts
-scp $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/$artifact
+scp -o StrictHostKeyChecking=no $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/$artifact
 
 # SSH the instance
 # ssh -oStrictHostKeyChecking=no ${VM_USER}@${VM_HOSTNAME}
