74eab9782ce465e15ff98eb3cdbce917b7e6dd68
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-06-18 10:52:55 +0100

    - Testing variable visiblity

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -10,7 +10,10 @@ artifact=proxy-${CI_ENVIRONMENT_SLUG}.war
 restart_command="cd ${VM_HOME_DIR} && bin/stop && bin/clean_dirs && cp deploy/$artifact webapps/ && bin/start"
 
 # Copy Artifacts
-scp $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/$artifact
+# scp $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/$artifact
+
+echo $VM_USER@$VM_HOSTNAME
+echo ${VM_USER}@${VM_HOSTNAME}
 
 # SSH the instance
 # ssh -oStrictHostKeyChecking=no ${VM_USER}@${VM_HOSTNAME}
