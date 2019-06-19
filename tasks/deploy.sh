69ab74ac6f7dc064f29fe670303b84f6e8c5eeaa
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-06-19 13:11:09 +0100

    - Test BETA deployment

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -9,10 +9,8 @@ artifact=proxy/build/libs/proxy-${CI_ENVIRONMENT_SLUG}.war
 # Command to be ran in the VM
 restart_command="cd ${VM_HOME_DIR} && bin/stop && bin/clean_dirs && cp deploy/submissions.war webapps/ && bin/start"
 
-echo ${VM_USER}@${VM_HOSTNAME}
-
 # Copy Artifacts
 scp -o StrictHostKeyChecking=no $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/submissions.war
 
 # SSH the instance
-ssh -oStrictHostKeyChecking=no ${VM_USER}@${VM_HOSTNAME} $restart_command
+# ssh -oStrictHostKeyChecking=no -v ${VM_USER}@${VM_HOSTNAME} $restart_command
