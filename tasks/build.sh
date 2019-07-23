ac60b5909c8fffc22671f7738eb36d778623501f
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-07-23 12:40:50 +0100

    - Update scripts

--- tasks/build.sh
+++ tasks/build.sh
@@ -4,7 +4,22 @@ set -e
 set -v
 
 # Clean build dist folder
-rm -rf server/public && mkdir server/public
+rm -rf dist && mkdir dist
+
+# Clean build dist folder
+rm -rf dist/public && mkdir dist/public
 
 # Build assets into dist/public folder
-npx ng build --outputPath=server/public --deleteOutputPath=true
+npx ng build --outputPath=dist/public --deleteOutputPath=true
+
+# Copy package.json to server
+cp package.json dist/
+
+# Install dependencies
+npm install --production --prefix dist
+
+# Copy server files
+cp -r server/ dist/
+
+# Copy config files
+cp -r config dist/
