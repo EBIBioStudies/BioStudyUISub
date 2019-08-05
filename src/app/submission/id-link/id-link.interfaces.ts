094ba61c9d29994fafb321033a01fcf9bb1bb489
Néstor Diaz <ndiaz@ebi.ac.uk>
2019-08-05 10:02:06 +0100

    Fix/identifiers namespaces (#221)
    - Get namespaces from new identifiers platform

--- src/app/submission/id-link/id-link.interfaces.ts
+++ src/app/submission/id-link/id-link.interfaces.ts
@@ -1,7 +1,7 @@
 export interface IdentifierNamespace {
   prefix: string;
-  name: string;
-  description: string;
+  name?: string;
+  description?: string;
 }
 
 export interface IdentifierEmbedded {
