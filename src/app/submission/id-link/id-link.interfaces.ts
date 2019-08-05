852c804025d91d9611462260977ac459d700676f
Néstor Diaz <ndiaz@ebi.ac.uk>
2019-08-05 09:29:24 +0100

    - Get namespaces from new identifiers platform (#220)

---
+++ src/app/submission/id-link/id-link.interfaces.ts
@@ -0,0 +1,13 @@
+export interface IdentifierNamespace {
+  prefix: string;
+  name: string;
+  description: string;
+}
+
+export interface IdentifierEmbedded {
+  namespaces: IdentifierNamespace[]
+}
+
+export interface IdentifierResponse {
+  _embedded: IdentifierEmbedded
+}
