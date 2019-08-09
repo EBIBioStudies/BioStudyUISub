082ab549cfe4fc7a91eb110007cc007913ae97a2
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-08-09 10:34:41 +0100

    - Test copying artifacts to dev environment

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -4,13 +4,13 @@ set -e
 set -v
 
 # Artifact name
-artifact=proxy/build/libs/proxy-${CI_ENVIRONMENT_SLUG}.war
+artifact=subtool-${CI_ENVIRONMENT_SLUG}.tar.gz
 
 # Command to be ran in the VM
-restart_command="cd ${VM_HOME_DIR} && bin/stop && bin/clean_dirs && cp deploy/${WAR_NAME}.war webapps/ && bin/start"
+# restart_command="cd ${VM_HOME_DIR} && bin/stop && bin/clean_dirs && cp deploy/${WAR_NAME}.war webapps/ && bin/start"
 
 # Copy Artifacts
-scp -o StrictHostKeyChecking=no $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/deploy/${WAR_NAME}.war
+scp -o StrictHostKeyChecking=no $artifact ${VM_USER}@${VM_HOSTNAME}:${VM_HOME_DIR}/node/subtool-${CI_ENVIRONMENT_SLUG}.tar.gz
 
 # SSH the instance
-ssh -oStrictHostKeyChecking=no -v ${VM_USER}@${VM_HOSTNAME} $restart_command
+# ssh -oStrictHostKeyChecking=no -v ${VM_USER}@${VM_HOSTNAME} $restart_command
