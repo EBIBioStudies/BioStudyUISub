62b7e13826a3e72ba41354b58afcedc6d929d863
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-08-16 10:03:30 +0100

    - Update aspera docs

--- src/app/file/file-list/ftp-upload-button/ftp-upload-button.component.ts
+++ src/app/file/file-list/ftp-upload-button/ftp-upload-button.component.ts
@@ -5,21 +5,38 @@ import { UserData } from 'app/auth/shared';
 @Component({
     selector: 'ftp-upload-button',
     templateUrl: './ftp-upload-button.component.html',
-    styleUrls:['./ftp-upload-button.component.css']
+    styleUrls: ['./ftp-upload-button.component.css']
 })
-
 export class FTPUploadButtonComponent {
-
     modalRef?: BsModalRef;
+    osName: string = 'unknown';
     secretId: string = '1234';
 
     constructor(
         private userData: UserData,
-        private modalService: BsModalService) {
+        private modalService: BsModalService
+    ) {
+        this.osName = this.getCurrentOSName();
+    }
+
+    getCurrentOSName() {
+        if (navigator.appVersion.indexOf('Win') !== -1) {
+            return 'windows';
+        }
+
+        if (navigator.appVersion.indexOf('Mac') !== -1) {
+            return 'macos';
+        }
+
+        if (navigator.appVersion.indexOf('Linux') !== -1) {
+            return 'linux';
+        }
+
+        return this.osName;
     }
 
     openModal(template: TemplateRef<any>) {
-        this.userData.secretId$.subscribe(secret => {
+        this.userData.secretId$.subscribe((secret) => {
             this.secretId = secret;
             this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
         });
