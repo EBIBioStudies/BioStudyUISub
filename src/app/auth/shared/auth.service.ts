a2f2a216ed05acff6149b7640b85a93ae54724a8
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-08-28 14:26:43 +0100

    - Migrate authentication flow to new backend

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -13,115 +13,104 @@ import {
     RegistrationData, copyAndExtend
 } from './model';
 
-
 interface StatusResponse {
     status: string, // 'OK' or 'FAIL'
     message?: string
 }
 
-interface UserInfoResponse extends UserInfo, StatusResponse {
-}
+interface UserInfoResponse extends UserInfo, StatusResponse { }
 
 @Injectable()
 export class AuthService {
-    private static statusCheck<T extends StatusResponse>(resp: T): T {
-        if (resp.status === 'OK') {
-            return resp;
-        }
-        throw ServerError.dataError(resp);
+  constructor(
+    private http: HttpClient,
+    private userSession: UserSession,
+    private appConfig: AppConfig
+  ) { }
+
+  signIn(user: { login: string, password: string }): Observable<UserInfo> {
+    return this.sendPostRequest<UserInfoResponse, UserInfo>('/raw/auth/signin', user)
+      .pipe(map((userInfo: UserInfo) => this.userSession.create(userInfo)));
+  }
+
+  checkUser(): Observable<UserInfo> {
+    return this.http.get<UserInfoResponse>(
+      '/raw/auth/profile',
+      { observe: 'response' }
+    ).pipe(
+      catchError((response: HttpErrorResponse) => this.catchError<UserInfoResponse>(response)),
+      map((response: HttpResponse<UserInfoResponse>) => this.checkStatus(response)),
+      map((user: UserInfo) => this.userSession.update(user))
+    );
+  }
+
+  passwordResetReq(obj: PasswordResetRequestData): Observable<StatusResponse> {
+    return this.sendPostRequest<UserInfoResponse, StatusResponse>('/raw/auth/changePassword', this.withInstanceKey(obj.snapshot()))
+      .pipe(() => of({ status: 'OK' }));
+  }
+
+  passwordReset(obj: PasswordResetData): Observable<StatusResponse> {
+    return this.http.post<StatusResponse>('/raw/auth/passreset', obj.snapshot());
+  }
+
+  activationLinkReq(obj: ActivationLinkRequestData): Observable<StatusResponse> {
+    return this.sendPostRequest<StatusResponse, StatusResponse>('/raw/auth/retryact', this.withInstanceKey(obj.snapshot()))
+      .pipe(() => of({ status: 'OK' }));
+  }
+
+  activate(key: string): Observable<StatusResponse> {
+    return this.http.post<StatusResponse>(`/raw/auth/activate/${key}`, {});
+  }
+
+  signUp(regData: RegistrationData): Observable<StatusResponse> {
+    return this.sendPostRequest<StatusResponse, StatusResponse>('/raw/auth/signup', this.withInstanceKey(regData.snapshot()))
+      .pipe(() => of({ status: 'OK' }));
+  }
+
+  signOut(): Observable<StatusResponse> {
+    if (this.userSession.isAnonymous()) {
+      return of({ status: 'OK' });
     }
 
-    private static catch403Error<T>(resp: HttpErrorResponse): Observable<T> {
-        if (resp.status === 403) {
-            return of(resp.error); // server should not return 403 status
-        }
+    return this.sendPostRequest<StatusResponse, StatusResponse>('/raw/auth/signout', { sessid: this.userSession.token() })
+      .pipe(
+        map(() => {
+          this.userSession.destroy();
 
-        throw resp;
+          return { status: 'OK' };
+        })
+      );
+  }
+
+  private catchError<T>(resp: HttpErrorResponse): Observable<T> {
+    if (resp.status === 403) {
+      return of(resp.error); // server should not return 403 status
     }
 
-    constructor(private http: HttpClient,
-                private userSession: UserSession,
-                private appConfig: AppConfig) {
+    throw ServerError.fromResponse(resp);
+  }
+
+  private checkStatus<R, T>(response: HttpResponse<R>): T {
+    if (response.status === 200) {
+      return <T>(response.body || {});
     }
 
-    private checkStatus<T>(response: HttpResponse<T>): UserInfo {
-        if (response.status === 200) {
-            return <UserInfo>(response.body || {});
-        }
+    throw ServerError.dataError(response.body);
+  }
 
-        throw ServerError.dataError(response.body);
-    }
+  private sendPostRequest<R, T>(path: string, payload: any): Observable<T> {
+    return this.http.post<R>(
+      path, payload, { observe: 'response' }
+    ).pipe(
+      catchError((response: HttpErrorResponse) => this.catchError<R>(response)),
+      map((response: HttpResponse<R>) => this.checkStatus<R, T>(response))
+    );
+  }
 
-    signIn(obj: { login: string, password: string }): Observable<UserInfo> {
-        return this.http.post<UserInfoResponse>(
-            '/raw/auth/signin',
-            obj,
-            { observe: 'response' }
-        ).pipe(
-            catchError((response: HttpErrorResponse) => AuthService.catch403Error<UserInfoResponse>(response)),
-            map((response: HttpResponse<UserInfoResponse>) => this.checkStatus<UserInfoResponse>(response)),
-            map((user: UserInfo) => this.userSession.create(user))
-        );
-    }
-
-    checkUser(): Observable<UserInfo> {
-        return this.http.get<UserInfoResponse>(
-            '/raw/auth/profile',
-            { observe: 'response' }
-        ).pipe(
-            map((response: HttpResponse<UserInfoResponse>) => this.checkStatus(response)),
-            map((user: UserInfo) => this.userSession.update(user))
-        );
-    }
-
-    passwordResetReq(obj: PasswordResetRequestData): Observable<StatusResponse> {
-        return this.http.post<StatusResponse>('/raw/auth/passrstreq', this.withInstanceKey(obj.snapshot())).pipe(
-            map(resp => AuthService.statusCheck(resp)));
-    }
-
-    passwordReset(obj: PasswordResetData): Observable<StatusResponse> {
-        return this.http.post<StatusResponse>('/raw/auth/passreset', obj.snapshot());
-    }
-
-    activationLinkReq(obj: ActivationLinkRequestData): Observable<StatusResponse> {
-        return this.http.post<StatusResponse>('/raw/auth/retryact', this.withInstanceKey(obj.snapshot()));
-    }
-
-    activate(key: string): Observable<StatusResponse> {
-        return this.http.post<StatusResponse>(`/raw/auth/activate/${key}`, {});
-    }
-
-    signUp(regData: RegistrationData): Observable<StatusResponse> {
-        return this.http.post<StatusResponse>(
-            '/raw/auth/signup',
-            this.withInstanceKey(regData.snapshot()),
-            { observe: 'response' }
-        ).pipe(
-            catchError((response) => AuthService.catch403Error<StatusResponse>(response)),
-            map((response: HttpResponse<StatusResponse>) => this.checkStatus(response)),
-            map(() => ({ status: 'OK' }))
-        );
-    }
-
-    signOut(): Observable<StatusResponse> {
-        if (this.userSession.isAnonymous()) {
-            return of({
-                status: 'OK'
-            });
-        }
-        return this.http.post<StatusResponse>('/raw/auth/signout', {sessid: this.userSession.token()}).pipe(
-            map(resp => AuthService.statusCheck(resp)),
-            map(resp => {
-                this.userSession.destroy();
-                return resp;
-            })
-        );
-    }
-
-    private withInstanceKey(obj: { [key: string]: string }): { [key: string]: string } {
-        return copyAndExtend(obj, {
-            instanceKey: this.appConfig.instanceKey,
-            path: this.appConfig.contextPath + '/' + obj.path
-        });
-    }
+  private withInstanceKey(obj: { [key: string]: string }): { [key: string]: string } {
+    return copyAndExtend(obj, {
+      instanceKey: this.appConfig.instanceKey,
+      path: this.appConfig.contextPath + '/' + obj.path
+    });
+  }
 }
