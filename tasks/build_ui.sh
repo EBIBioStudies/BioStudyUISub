622e9a2cf2f35b579eef07d3dfb4ef9b82beb22e
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-09-13 13:38:38 +0100

    - Remove unused config params

--- tasks/build_ui.sh
+++ tasks/build_ui.sh
@@ -9,14 +9,12 @@ rm -rf dist && mkdir dist
 # Config properties
 appProxyBase=${APP_PROXY_BASE};
 appContext=${APP_CONTEXT};
-appDebugEnabled=${APP_DEBUG_ENABLED};
 appInstanceKey=${APP_INSTANCE_KEY};
 ciEnvironment=${CI_ENVIRONMENT_SLUG};
 
 # Update config.json
 sed -i 's%"APP_PROXY_BASE":.*%"APP_PROXY_BASE":"'$appProxyBase'",%' src/config.json
 sed -i 's%"APP_CONTEXT":.*%"APP_CONTEXT":"'$appContext'",%' src/config.json
-sed -i 's%"APP_DEBUG_ENABLED":.*%"APP_DEBUG_ENABLED":'$appDebugEnabled',%' src/config.json
 # Last line, don't append a comma
 sed -i 's%"APP_INSTANCE_KEY".*%"APP_INSTANCE_KEY":"'$appInstanceKey'"%' src/config.json
 
