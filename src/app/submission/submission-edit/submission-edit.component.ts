1507573fc547523ffd988811b92262a12aeb53df
Awais Athar <awais@ebi.ac.uk>
2019-09-17 15:20:17 +0100

    [Delivers #160256850] Show a message on release date change (#254)

--- src/app/submission/submission-edit/submission-edit.component.ts
+++ src/app/submission/submission-edit/submission-edit.component.ts
@@ -43,7 +43,7 @@ export class SubmissionEditComponent implements OnInit, OnDestroy, AfterViewChec
 
     private accno?: string;
     private hasJustCreated = false;
-    private releaseDate = '';
+    private releaseDate: Date = new Date();
     private scrollToCtrl?: FormControl;
     private sideBarCollapsed = false;
     private submitOperation: SubmitOperation = SubmitOperation.Unknown;
@@ -124,6 +124,10 @@ export class SubmissionEditComponent implements OnInit, OnDestroy, AfterViewChec
                     ).subscribe(() => {
                         this.router.navigate(['/submissions/']);
                     });
+                } else {
+                    const releaseDateCtrl = this.sectionForm!.findFieldControl('ReleaseDate');
+                    this.releaseDate =  releaseDateCtrl ? new Date (Date.parse(releaseDateCtrl.control.value)) : new Date();
+                    this.releaseDate.setHours(0, 0, 0);
                 }
             });
     }
@@ -164,10 +168,11 @@ export class SubmissionEditComponent implements OnInit, OnDestroy, AfterViewChec
             return;
         }
 
-        (isConfirm ? this.confirmSubmit() : of(true))
-            .pipe(
-                filter(v => v),
-                switchMap(() => this.submEditService.submit())
+            (isConfirm ? this.confirmSubmit() : of(true))
+                .pipe(
+                    filter(v => v),
+                    switchMap( () => this.confirmReleaseDateOverride()),
+                    switchMap(() => this.submEditService.submit())
             ).takeUntil(this.unsubscribe)
             .subscribe(resp => this.onSubmitFinished(resp));
     }
@@ -271,6 +276,20 @@ export class SubmissionEditComponent implements OnInit, OnDestroy, AfterViewChec
             'Submit the study',
             'Submit',
         );
+    }
+
+    private confirmReleaseDateOverride(): Observable<boolean> {
+        const releaseDateCtrl = this.sectionForm!.findFieldControl('ReleaseDate');
+        const newReleaseDate = releaseDateCtrl ? new Date (Date.parse(releaseDateCtrl.control.value)) : new Date();
+        newReleaseDate.setHours(0, 0, 0);
+        const today = new Date();
+        today.setHours(0, 0, 0);
+        return (this.releaseDate <= today && newReleaseDate > today) ? this.modalService.whenConfirmed(
+            'This study has already been released and resetting the release date may make it ' +
+            'unavailable to the public. Are you sure you want to continue?',
+            'Submit the study',
+            'OK',
+        ) : of(true);
     }
 
     private confirmPageDelete(message: string): Observable<boolean> {
