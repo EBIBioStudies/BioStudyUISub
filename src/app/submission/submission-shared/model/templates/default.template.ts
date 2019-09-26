4d26a92d1679c5904dfb35ddb171a1dae936eb5f
Nestor Diaz <ndiaz@ebi.ac.uk>
2019-09-26 15:22:08 +0100

    - Make File section visible by default in the default template

--- src/app/submission/submission-shared/model/templates/default.template.ts
+++ src/app/submission/submission-shared/model/templates/default.template.ts
@@ -248,6 +248,7 @@ export const DefaultTemplate = {
                     'are supported. URLs must include the scheme, e.g. "http://".',
                 'icon': 'fa-link',
                 'uniqueCols': true,
+                'display': 'required',
                 'columnTypes': [
                     {
                         'name': 'Pointer',
@@ -267,6 +268,7 @@ export const DefaultTemplate = {
                 'description': 'List the data files for the study and describe their respective scopes.',
                 'icon': 'fa-file',
                 'uniqueCols': true,
+                'display': 'required',
                 'columnTypes': [
                     {
                         'name': 'Path',
