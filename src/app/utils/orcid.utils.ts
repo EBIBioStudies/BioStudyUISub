7dd6856c6df0ec90df29b43558268e814d612b1c
Néstor Diaz <ndiaz@ebi.ac.uk>
2019-12-04 16:05:27 +0000

    Improve ORCID validation (#295)
    - Improve ORCID validation

---
+++ src/app/utils/orcid.utils.ts
@@ -0,0 +1,19 @@
+export function isOrcidValid(orcid) {
+  const orcidFormatRegex: RegExp = /\d{4}-\d{4}-\d{4}-\d{3}[\dX]/gi;
+  const matches: string[] = orcid.match(orcidFormatRegex);
+
+  if (matches === null) {
+    return false;
+  }
+
+  const digits: string = orcid.replace(/-/g, '');
+  const baseDigits: string[] = digits.slice(0, 15).split('');
+  const checkDigit: string = digits.charAt(15);
+
+  const total: number = baseDigits.reduce((parcialTotal, digit) => (parcialTotal + parseInt(digit, 10)) * 2, 0);
+  const remainder: number = total % 11;
+  const result: number = (12 - remainder) % 11;
+  const expectedCheckDigit: string = result === 10 ? 'X' : result.toString();
+
+  return checkDigit === expectedCheckDigit;
+}
