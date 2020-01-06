fc8db7eaf3fc424d61172e7c1fcf923cd0f1924f
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-01-06 14:02:22 +0000

    - Update ENV VARS names

--- tasks/build_setup.sh
+++ tasks/build_setup.sh
@@ -10,8 +10,8 @@ rm -rf dist && mkdir dist
 rm -rf dist/public && mkdir dist/public
 
 # Config properties
-appContext=${APP_CONTEXT};
-appInstanceKey=${APP_INSTANCE_KEY};
+contextPath=${CONTEXT_PATH};
+instanceKey=${INSTANCE_KEY};
 appPort=${PORT}
 appProxyBase=${APP_PROXY_BASE};
 backendHostName=${BACKEND_HOST_NAME}
@@ -21,9 +21,9 @@ ciEnvironment=${CI_ENVIRONMENT_SLUG};
 
 # Update config.json
 sed -i 's%"APP_PROXY_BASE":.*%"APP_PROXY_BASE":"'$appProxyBase'",%' src/config.json
-sed -i 's%"APP_CONTEXT":.*%"APP_CONTEXT":"'$appContext'",%' src/config.json
+sed -i 's%"CONTEXT_PATH":.*%"CONTEXT_PATH":"'$contextPath'",%' src/config.json
 # Last line, don't append a comma
-sed -i 's%"APP_INSTANCE_KEY".*%"APP_INSTANCE_KEY":"'$appInstanceKey'"%' src/config.json
+sed -i 's%"INSTANCE_KEY".*%"INSTANCE_KEY":"'$instanceKey'"%' src/config.json
 
 # Create .env file
 echo "
