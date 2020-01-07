c66394b1a52adcaa759b2a704424b49f8ea0ee24
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-01-07 09:32:48 +0000

    - Include contextPath in static resources path

--- tasks/build.sh
+++ tasks/build.sh
@@ -4,10 +4,16 @@ set -e
 set -v
 
 # Variables
-ciEnvironment=${CI_ENVIRONMENT_SLUG};
+ciEnvironment=${CI_ENVIRONMENT_SLUG}
+
+if [ -z "${CONTEXT_PATH}" ]; then
+  resources="/static/"
+else
+  resources="/${CONTEXT_PATH}/static/"
+fi
 
 # Build assets into dist/public folder
-npx ng build --outputPath=dist/public --deleteOutputPath=true --baseHref=/static/
+npx ng build --outputPath=dist/public --deleteOutputPath=true --baseHref=${resources}
 
 # Copy package.json to server
 cp package.json dist/
