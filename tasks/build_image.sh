5627d3723369c7d525fdc261798e9ff424adb867
Néstor Diaz <ndiaz@ebi.ac.uk>
2020-04-03 10:27:07 +0100

    - Build and publish docker image (#330)

---
+++ tasks/build_image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Creates the Docker image for this project and pushes it to GitLab registry.
+
+set -e
+set -v
+
+# Variables
+artifact=subtool-${CI_ENVIRONMENT_SLUG}.tar.gz
+
+# Create a folder to uncompressed the artifacts
+mkdir dist
+
+# Copy the artifact generated in the build stage
+if [ -f $artifact ]; then
+  tar -xvf $artifact -C ./dist;
+fi
+
+# Login to the GitLab Docker registry
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+
+# Build Docker image
+docker build -t $CI_REGISTRY/ndiaz/biostudyuisub:latest .
+
+# Push image to docker registry
+docker push $CI_REGISTRY/ndiaz/biostudyuisub:latest
+
+# Logout from the registry
+docker logout $CI_REGISTRY
