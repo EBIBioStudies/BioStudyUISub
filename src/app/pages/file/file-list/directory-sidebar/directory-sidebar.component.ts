5fcbc533bc8c9de83b3c75da062f65603d2f042b
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-04-21 14:41:47 +0100

    - Add a "Group" title in the directories sidebar

--- src/app/pages/file/file-list/directory-sidebar/directory-sidebar.component.ts
+++ src/app/pages/file/file-list/directory-sidebar/directory-sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { FileService } from '../../shared/file.service';
+import { PathInfo } from '../../shared/file-rest.model';
 import { FileNode } from 'app/pages/file/file-select/file-tree.model';
 import { map } from 'rxjs/operators';
 
@@ -14,9 +15,10 @@ import { map } from 'rxjs/operators';
 })
 export class DirectorySidebarComponent implements OnInit, ControlValueAccessor {
   @Input() collapsed?: boolean = false;
-  dirs: FileNode[] = [];
+  groups: FileNode[] = [];
   @Output() select = new EventEmitter();
   @Output() toggle = new EventEmitter();
+  userDirs: FileNode[] = [];
 
   private selectedPath?: string;
 
@@ -32,10 +34,14 @@ export class DirectorySidebarComponent implements OnInit, ControlValueAccessor {
   }
 
   ngOnInit() {
-    this.fileService.getUserDirs().pipe(
-      map((dirs) => dirs.map((dir) => new FileNode(true, dir.path, dir.name)))
-    ).subscribe((dirs) => {
-      this.dirs = dirs;
+    const userDir: PathInfo = this.fileService.getUserDir();
+
+    this.userDirs = [new FileNode(true, userDir.path, userDir.name)];
+
+    this.fileService.getUserGroups().pipe(
+      map((groups) => groups.map((group) => new FileNode(true, 'groups', group.name)))
+    ).subscribe((groups) => {
+      this.groups = groups;
     });
   }
 
