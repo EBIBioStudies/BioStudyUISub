37a218bbddf052b5fb38b50d7d1fc7c3930c374c
Néstor Diaz <ndiaz@ebi.ac.uk>
2020-08-18 09:15:45 +0100

    Allow selecting Aspera options per OS (#398)

--- src/app/pages/file/file-list/ftp-upload-button/ftp-upload-button.component.ts
+++ src/app/pages/file/file-list/ftp-upload-button/ftp-upload-button.component.ts
@@ -2,6 +2,12 @@ import { Component, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 import { UserData } from 'app/auth/shared';
 
+interface OSOption {
+  displayName: string,
+  link: string,
+  name: string
+}
+
 @Component({
   selector: 'st-ftp-upload-button',
   templateUrl: './ftp-upload-button.component.html',
@@ -9,33 +15,43 @@ import { UserData } from 'app/auth/shared';
 })
 export class FTPUploadButtonComponent {
   modalRef?: BsModalRef;
-  osName: string = 'unknown';
+  osOption: OSOption;
+  osOptions: OSOption[] = [{
+    name: 'Win',
+    displayName: 'Windows',
+    link: 'https://www.ibm.com/support/fixcentral/swg/selectFixes' +
+          '?parent=ibm%7EOther%20software&product=ibm/Other+software/IBM+Aspera+CLI&release=All&platform=Windows&function=all'
+  }, {
+    name: 'Mac',
+    displayName: 'Mac OS',
+    link: 'https://www.ibm.com/support/fixcentral/swg/selectFixes' +
+          '?parent=ibm%7EOther%20software&product=ibm/Other+software/IBM+Aspera+CLI&release=All&platform=Mac+OSX&function=all'
+  }, {
+    name: 'Linux',
+    displayName: 'Linux',
+    link: 'https://www.ibm.com/support/fixcentral/swg/selectFixes' +
+          '?parent=ibm%7EOther%20software&product=ibm/Other+software/IBM+Aspera+CLI&release=All&platform=Linux&function=all'
+  }];
   secretId: string = '1234';
 
   constructor(
     private userData: UserData,
     private modalService: BsModalService
   ) {
-    this.osName = this.getCurrentOSName();
+    this.osOption = this.getCurrentOS();
   }
 
-  getCurrentOSName() {
-    if (navigator.appVersion.indexOf('Win') !== -1) {
-      return 'windows';
-    }
+  getCurrentOS(): OSOption {
+    const currentOS: OSOption | undefined = this.osOptions.find((option) => navigator.appVersion.indexOf(option.name) !== -1);
 
-    if (navigator.appVersion.indexOf('Mac') !== -1) {
-      return 'macos';
-    }
-
-    if (navigator.appVersion.indexOf('Linux') !== -1) {
-      return 'linux';
-    }
-
-    return this.osName;
+    return currentOS === undefined ? this.osOptions[0] : currentOS;
   }
 
-  openModal(template: TemplateRef<any>) {
+  onOSChange(optionName): void {
+    this.osOption = this.osOptions.find((option) => option.name === optionName) ||  this.osOptions[0];
+  }
+
+  openModal(template: TemplateRef<any>): void {
     this.userData.secretId$.subscribe((secret) => {
       this.secretId = secret;
       this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
