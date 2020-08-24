bade441cf3107d7de9a039ef10482f97beb590fb
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-08-24 10:38:48 +0100

    Fix unit tests

---
+++ src/tests/setupJestAfterEnv.ts
@@ -0,0 +1,5 @@
+import 'jest-preset-angular';
+import '@testing-library/jest-dom/extend-expect';
+import './mocks/jestGlobalMocks';
+import '../app/extensions/array.extensions';
+import '../app/extensions/string.extensions';
