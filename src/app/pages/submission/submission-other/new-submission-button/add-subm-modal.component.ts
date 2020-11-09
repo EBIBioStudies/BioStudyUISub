2ee42640d957cb9834d7e55e6d6eeb11f1a9fb04
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-11-09 09:39:04 +0000

    Fix error focusing template in add submission modal

--- src/app/pages/submission/submission-other/new-submission-button/add-subm-modal.component.ts
+++ src/app/pages/submission/submission-other/new-submission-button/add-subm-modal.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'st-add-subm-modal',
   templateUrl: './add-subm-modal.component.html'
 })
-export class AddSubmModalComponent implements AfterViewInit {
+export class AddSubmModalComponent {
   onOk?: (selected: string) => void;
   selected: string = 'Default';
   templates?: Array<{ description: string, name: string }> = [];
@@ -14,10 +14,6 @@ export class AddSubmModalComponent implements AfterViewInit {
 
   hide(): void {
     this.bsModalRef.hide();
-  }
-
-  ngAfterViewInit(): void {
-    (document.getElementsByClassName('project-radio')[0] as HTMLInputElement).focus();
   }
 
   ok(): void {
