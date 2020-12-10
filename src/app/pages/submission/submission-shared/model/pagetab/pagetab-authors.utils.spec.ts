2fdadbb7cc9852cc2fd639ee9047a94494a520b0
Nestor Diaz <ndiaz@ebi.ac.uk>
2020-12-10 11:21:39 +0000

    Fix unit test

--- src/app/pages/submission/submission-shared/model/pagetab/pagetab-authors.utils.spec.ts
+++ src/app/pages/submission/submission-shared/model/pagetab/pagetab-authors.utils.spec.ts
@@ -2,8 +2,8 @@ import { authorsToContacts, contactsToAuthors } from './pagetab-authors.utils';
 
 describe('AuthorsAndAffiliations:', () => {
   it('authorsToContacts: authors and affiliations are merged into contacts', () => {
-    expect(authorsToContacts(
-      [
+    expect(
+      authorsToContacts([
         {
           type: 'Author',
           attributes: [
@@ -54,157 +54,151 @@ describe('AuthorsAndAffiliations:', () => {
           type: 'Other',
           attributes: []
         }
-      ])).toEqual(
-      [
-        {
-          type: 'Other',
-          attributes: []
-        },
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'John D'
-            },
-            {
-              name: 'Organisation',
-              value: ['EMBL-EBI']
-            }
-          ]
-        },
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Guy R'
-            },
-            {
-              name: 'Organisation',
-              value: ['Some organisation']
-            }
-          ]
-        },
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Bob D'
-            }
-          ]
-        }
-      ]);
+      ])
+    ).toEqual([
+      {
+        type: 'Other',
+        attributes: []
+      },
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'John D'
+          },
+          {
+            name: 'Organisation',
+            value: ['EMBL-EBI']
+          }
+        ]
+      },
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Guy R'
+          },
+          {
+            name: 'Organisation',
+            value: ['Some organisation']
+          }
+        ]
+      },
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Bob D'
+          }
+        ]
+      }
+    ]);
   });
 
   it('contactsToAuthors: [contact] sections are split into authors and affiliations', () => {
-    const authors = contactsToAuthors(
-      [
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'John D'
-            },
-            {
-              accno: 'o1',
-              name: 'Organisation',
-              value: 'Org1'
-            }]
-        },
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Bob D'
-            },
-            {
-              accno: 'o1',
-              name: 'Organisation',
-              value: 'Org1'
-            }
-          ]
-        },
-        {
-          type: 'Contact',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Guy R'
-            },
-            {
-              name: 'Organisation',
-              value: ''
-            }
-          ]
-        },
-        {
-          type: 'Other',
-          attributes: []
-        }]);
+    const authors = contactsToAuthors([
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'John D'
+          },
+          {
+            accno: 'o1',
+            name: 'Organisation',
+            value: 'Org1'
+          }
+        ]
+      },
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Bob D'
+          },
+          {
+            accno: 'o1',
+            name: 'Organisation',
+            value: 'Org1'
+          }
+        ]
+      },
+      {
+        type: 'Contact',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Guy R'
+          },
+          {
+            name: 'Organisation',
+            value: ''
+          }
+        ]
+      },
+      {
+        type: 'Other',
+        attributes: []
+      }
+    ]);
 
-    expect(authors).toEqual(
-      [
-        {
-          type: 'Other',
-          attributes: []
-        },
-        {
-          type: 'Author',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'John D'
-            },
-            {
-              name: 'affiliation',
-              reference: true,
-              value: 'o1'
-            }
-          ]
-        },
-        {
-          type: 'Author',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Bob D'
-            },
-            {
-              name: 'affiliation',
-              reference: true,
-              value: 'o1'
-            }
-          ]
-        },
-        {
-          type: 'Author',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Guy R'
-            },
-            {
-              name: 'affiliation',
-              value: ''
-            }
-          ]
-        },
-        {
-          type: 'Organization',
-          accno: 'o1',
-          attributes: [
-            {
-              name: 'Name',
-              value: 'Org1'
-            }
-          ]
-        }
-      ]
-    );
-
+    expect(authors).toEqual([
+      {
+        type: 'Other',
+        attributes: []
+      },
+      {
+        type: 'Author',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'John D'
+          },
+          {
+            name: 'affiliation',
+            reference: true,
+            value: 'o1'
+          }
+        ]
+      },
+      {
+        type: 'Author',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Bob D'
+          },
+          {
+            name: 'affiliation',
+            reference: true,
+            value: 'o1'
+          }
+        ]
+      },
+      {
+        type: 'Author',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Guy R'
+          }
+        ]
+      },
+      {
+        type: 'Organization',
+        accno: 'o1',
+        attributes: [
+          {
+            name: 'Name',
+            value: 'Org1'
+          }
+        ]
+      }
+    ]);
   });
 });
