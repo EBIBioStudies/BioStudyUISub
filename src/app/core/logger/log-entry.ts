87a2a8c527bc5c60b9e2fb719c6860d2ae0555b2
Néstor Diaz <ndiaz@ebi.ac.uk>
2021-04-07 04:53:00 -0500

    Fix regex validator for links (#471)

--- src/app/core/logger/log-entry.ts
+++ src/app/core/logger/log-entry.ts
@@ -7,12 +7,6 @@ interface LogJsonFormat {
   stackTrace: string;
 }
 
-interface FileParams {
-  name: string;
-  size: number;
-  type: string;
-}
-
 export class LogEntry {
   constructor(
     public message: string = '',
@@ -35,19 +29,5 @@ export class LogEntry {
   buildLogString(): string {
     const level: string = LogLevel[this.level];
     return `${new Date()} [${level.toLowerCase()}] - ${this.userEmail} - ${this.message} - ${this.stackTrace}`;
-  }
-
-  private formatFileParams(files: File[]): FileParams[] {
-    return files.map((file) => ({ name: file.name, size: file.size, type: file.type }));
-  }
-
-  private formatParams(params: any[]): string {
-    const hasObject = params.some((param) => typeof param === 'object');
-
-    if (hasObject) {
-      return params.reduce((formattedParams, currentParam) => `${formattedParams}${JSON.stringify(currentParam)},`, '');
-    }
-
-    return params.join(',');
   }
 }
