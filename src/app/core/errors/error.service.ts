6b5c6e8706dacfc5dd3fc83679d8d502135e5dcd
Néstor Diaz <ndiaz@ebi.ac.uk>
2021-05-07 10:20:01 -0500

    Allow editing release date from input (#479)

--- src/app/core/errors/error.service.ts
+++ src/app/core/errors/error.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NOT_FOUND, INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import { ServerError } from 'app/shared/server-error.handler';
+import { ErrorMessageService } from './error-message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
+  constructor(private errorMessageService: ErrorMessageService) {}
+
   getClientErrorMessage(error: Error): string {
     return error.message ? error.message : error.toString();
   }
@@ -20,7 +23,9 @@ export class ErrorService {
       return ServerError.defaultErrorMessage;
     }
 
-    return navigator.onLine ? resp?.error?.log?.message : 'Looks like there is no internet connection';
+    const errorMessage = resp?.error?.log?.message || this.errorMessageService.getMessage();
+
+    return navigator.onLine ? errorMessage : 'Looks like there is no internet connection';
   }
 
   isNotFoundError(resp: HttpErrorResponse): boolean {
