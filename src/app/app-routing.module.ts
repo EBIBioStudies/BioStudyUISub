2d3fb1696e6ec302bee52e97bb848a60418543e8
Néstor Diaz <ndiaz@ebi.ac.uk>
2021-06-08 04:27:17 -0500

    Change reset page wording when user is seting up password (#488) (#490)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const appRoutes: Routes = [
   { path: 'activate/:key', component: ActivateComponent },
   { path: 'password_reset_request', component: PasswordResetReqComponent },
   { path: 'password_reset/:key', component: PasswordResetComponent },
+  { path: 'password_setup/:key', component: PasswordResetComponent, data: { isPassSetup: true } },
   { path: 'resend_activation_link', component: ActivationLinkReqComponent },
   { path: '', component: SignInComponent },
   { path: '**', component: NotFoundComponent }
