1dd439d67877afc88041c177ebc77cdf51258017
Nestor Diaz <ndiaz@ebi.ac.uk>
2021-08-03 14:18:27 +0100

    Fix autofocus in firefox

--- src/app/auth/activate/activation-link-success.component.ts
+++ src/app/auth/activate/activation-link-success.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'st-activation-link-success',
   templateUrl: './activation-link-success.component.html'
 })
-export class ActivationLinkSuccessComponent {
+export class ActivationLinkSuccessComponent implements AfterViewInit {
   @Input() email: string = '';
   @Input() comesFromEmail: boolean = false;
   activationCode: string = '';
+
+  @ViewChild('activationCodeRef', { static: false })
+  private activationCodeRef!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.activationCodeRef.nativeElement.focus();
+  }
 
   get isActivationCodeEmpty(): boolean {
     return this.activationCode.length === 0;
