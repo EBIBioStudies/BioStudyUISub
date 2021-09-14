871927ee334572e9dcebff49ccfd4f418be73874
Néstor Diaz <ndiaz@ebi.ac.uk>
2021-09-14 09:54:59 +0100

    Show validation error at section level (#541)

--- src/app/submission/submission-edit/subm-navbar/subm-validation-errors.component.ts
+++ src/app/submission/submission-edit/subm-navbar/subm-validation-errors.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { SubmValidationErrors } from 'app/submission/submission-shared/model';
+import { SectionValidationError, SubmValidationErrors } from 'app/submission/submission-shared/model';
 
 @Component({
   selector: 'st-subm-validation-errors',
@@ -8,7 +8,15 @@ import { SubmValidationErrors } from 'app/submission/submission-shared/model';
 export class SubmValidationErrorsComponent {
   @Input() errors: SubmValidationErrors = SubmValidationErrors.EMPTY;
 
-  get errorList(): Array<string> {
-    return this.errors.errors;
+  get hasValidationErrors(): boolean {
+    return this.errors.total() > 0;
+  }
+
+  get sectionErrors(): Array<string> {
+    const errors: (string | SectionValidationError)[] = this.errors.errors.filter(
+      (error) => error instanceof SectionValidationError
+    );
+
+    return errors.map((error) => (error as SectionValidationError).message);
   }
 }
