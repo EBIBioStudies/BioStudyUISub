5e636457c09bbaafd55b68c858b0ee0e3201ef2d
Nestor Diaz <ndiaz@ebi.ac.uk>
2021-11-12 10:12:19 +0000

    Singup component tests

--- src/app/auth/auth.spec.ts
+++ src/app/auth/auth.spec.ts
@@ -3,22 +3,43 @@ import { fireEvent, render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RecaptchaModule } from 'ng-recaptcha';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ThemeModule } from 'app/theme/theme.module';
+import { SharedModule } from 'app/shared/shared.module';
 import { AuthComponent } from './auth.component';
 import { authRoutes } from './auth-routing.module';
 import { AuthService } from './shared/auth.service';
 import { UserSession } from './shared/user-session';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 import { rest, server } from './../../tests/server';
 
 test('is is possible to sign in, sign up, reset password and activate account', async () => {
   const { navigate } = await render(AuthComponent, {
-    imports: [FormsModule, HttpClientModule, ThemeModule, ModalModule.forRoot()],
+    imports: [SharedModule, RecaptchaModule, FormsModule, HttpClientModule, ThemeModule, ModalModule.forRoot()],
     providers: [AuthService, UserSession],
-    declarations: [SignInComponent],
+    declarations: [SignInComponent, SignUpComponent],
     routes: authRoutes
   });
+
+  // Test signup
+  await navigate('/signup');
+
+  // const signUpSubmitButton = screen.getByRole('button', { name: /register/i });
+  const signUpNameInput = screen.getByRole('textbox', { name: /name/i });
+  const signUpEmailInput = screen.getByRole('textbox', { name: /email/i });
+  const signUpPasswordInput = screen.getByLabelText(/password/i);
+  const signUpOrcidInput = screen.getByRole('textbox', { name: /orcid/i });
+
+  fireEvent.blur(signUpNameInput);
+  fireEvent.blur(signUpEmailInput);
+  fireEvent.blur(signUpPasswordInput);
+
+  expect(signUpNameInput).toBeInvalid();
+  expect(signUpEmailInput).toBeInvalid();
+  expect(signUpPasswordInput).toBeInvalid();
+  expect(signUpOrcidInput).toBeValid();
 
   // Test signin
   await navigate('/signin');
@@ -26,7 +47,6 @@ test('is is possible to sign in, sign up, reset password and activate account', 
   const submitButton = screen.getByRole('button', { name: /log in/i });
   const emailInput = screen.getByRole('textbox', { name: /email/i });
   const passwordInput = screen.getByLabelText(/password/i);
-  // const signInErrorMessage = 'Please check the fields below and try again';
 
   fireEvent.blur(emailInput);
   fireEvent.blur(passwordInput);
