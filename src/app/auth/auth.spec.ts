2d2ad5cc96ed15903b61b8e658eb3ad37cbe851d
Nestor Diaz <ndiaz@ebi.ac.uk>
2021-11-18 13:46:15 +0000

    Continue signup testing

--- src/app/auth/auth.spec.ts
+++ src/app/auth/auth.spec.ts
@@ -3,7 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RecaptchaModule } from 'ng-recaptcha';
+import { RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ThemeModule } from 'app/theme/theme.module';
 import { SharedModule } from 'app/shared/shared.module';
@@ -17,20 +17,36 @@ import { rest, server } from './../../tests/server';
 
 test('is is possible to sign in, sign up, reset password and activate account', async () => {
   const { navigate } = await render(AuthComponent, {
-    imports: [SharedModule, RecaptchaModule, FormsModule, HttpClientModule, ThemeModule, ModalModule.forRoot()],
-    providers: [AuthService, UserSession],
+    imports: [
+      SharedModule,
+      FormsModule,
+      HttpClientModule,
+      ThemeModule,
+      ModalModule.forRoot(),
+      RecaptchaModule,
+      RecaptchaFormsModule
+    ],
+    providers: [
+      AuthService,
+      UserSession,
+      {
+        provide: RECAPTCHA_SETTINGS,
+        useValue: { siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', size: 'invisible' } as RecaptchaSettings
+      }
+    ],
     declarations: [SignInComponent, SignUpComponent],
     routes: authRoutes
   });
 
-  // Test signup
+  // TEST SINGUP
   await navigate('/signup');
 
-  // const signUpSubmitButton = screen.getByRole('button', { name: /register/i });
+  const signUpSubmitButton = screen.getByRole('button', { name: /register/i });
   const signUpNameInput = screen.getByRole('textbox', { name: /name/i });
   const signUpEmailInput = screen.getByRole('textbox', { name: /email/i });
   const signUpPasswordInput = screen.getByLabelText(/password/i);
   const signUpOrcidInput = screen.getByRole('textbox', { name: /orcid/i });
+  const signUpTermsCheck = screen.getByRole('checkbox');
 
   fireEvent.blur(signUpNameInput);
   fireEvent.blur(signUpEmailInput);
@@ -39,9 +55,34 @@ test('is is possible to sign in, sign up, reset password and activate account', 
   expect(signUpNameInput).toBeInvalid();
   expect(signUpEmailInput).toBeInvalid();
   expect(signUpPasswordInput).toBeInvalid();
+  expect(signUpSubmitButton).toBeDisabled();
+  expect(signUpOrcidInput).toBeValid();
+  expect(screen.getByText('Please enter your full name')).toBeInTheDocument();
+  expect(screen.getByText('Please enter a valid email')).toBeInTheDocument();
+  expect(screen.getByText('Please enter your new password')).toBeInTheDocument();
+
+  // Test ORCID validation
+  userEvent.type(signUpOrcidInput, '1234');
+  expect(signUpOrcidInput).toBeInvalid();
+
+  userEvent.clear(signUpOrcidInput);
+  userEvent.type(signUpOrcidInput, '0000-0002-1825-0097');
   expect(signUpOrcidInput).toBeValid();
 
-  // Test signin
+  // Test signup flow
+  userEvent.type(signUpNameInput, 'Pep Sanchez');
+  userEvent.type(signUpEmailInput, 'pep@ebi.ac.uk');
+  userEvent.type(signUpPasswordInput, '123456');
+  userEvent.click(signUpTermsCheck);
+
+  // After terms has been accepted Register button should be enabled
+  expect(signUpSubmitButton).toBeEnabled();
+
+  screen.debug();
+
+  userEvent.click(signUpSubmitButton);
+
+  // TEST SIGNIN
   await navigate('/signin');
 
   const submitButton = screen.getByRole('button', { name: /log in/i });
--- src/tests/server.ts
+++ src/tests/server.ts
@@ -6,6 +6,7 @@ import { collections } from './fixtures/collections';
 
 // This configures a request mocking server with the given request handlers.
 const server = setupServer(
+  rest.post('/api/auth/register', (_, res, ctx) => res(ctx.status(OK))),
   rest.post('/api/auth/login', (_, res, ctx) => res(ctx.status(OK), ctx.json(userProfile))),
   rest.post('/api/auth/activate/:key', (_, res, ctx) => res(ctx.status(OK))),
   rest.get('/api/auth/profile', (_, res, ctx) => res(ctx.status(OK), ctx.json(userProfile))),
