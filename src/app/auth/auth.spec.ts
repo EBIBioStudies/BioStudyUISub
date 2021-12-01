b7cf4b7bf84134e1111b1eba1cc8035247674b9b
Nestor Diaz <ndiaz@ebi.ac.uk>
2021-12-01 14:28:43 +0000

    Mock recaptcha

--- src/app/auth/auth.spec.ts
+++ src/app/auth/auth.spec.ts
@@ -1,12 +1,13 @@
-import { UNAUTHORIZED } from 'http-status-codes';
+import { UNAUTHORIZED, BAD_REQUEST } from 'http-status-codes';
 import { fireEvent, render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ThemeModule } from 'app/theme/theme.module';
 import { SharedModule } from 'app/shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { MockModule } from './../../tests/mocks/mock.module';
 import { AuthComponent } from './auth.component';
 import { authRoutes } from './auth-routing.module';
 import { AuthService } from './shared/auth.service';
@@ -18,22 +19,15 @@ import { rest, server } from './../../tests/server';
 test('is is possible to sign in, sign up, reset password and activate account', async () => {
   const { navigate } = await render(AuthComponent, {
     imports: [
+      CommonModule,
       SharedModule,
       FormsModule,
       HttpClientModule,
       ThemeModule,
       ModalModule.forRoot(),
-      RecaptchaModule,
-      RecaptchaFormsModule
+      MockModule
     ],
-    providers: [
-      AuthService,
-      UserSession,
-      {
-        provide: RECAPTCHA_SETTINGS,
-        useValue: { siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', size: 'invisible' } as RecaptchaSettings
-      }
-    ],
+    providers: [AuthService, UserSession],
     declarations: [SignInComponent, SignUpComponent],
     routes: authRoutes
   });
@@ -46,7 +40,8 @@ test('is is possible to sign in, sign up, reset password and activate account', 
   const signUpEmailInput = screen.getByRole('textbox', { name: /email/i });
   const signUpPasswordInput = screen.getByLabelText(/password/i);
   const signUpOrcidInput = screen.getByRole('textbox', { name: /orcid/i });
-  const signUpTermsCheck = screen.getByRole('checkbox');
+  const signUpTermsCheck = screen.getByRole('checkbox', { name: /terms/i });
+  const signUpRecaptcha = screen.getByRole('checkbox', { name: /recaptcha/i });
 
   fireEvent.blur(signUpNameInput);
   fireEvent.blur(signUpEmailInput);
@@ -69,18 +64,38 @@ test('is is possible to sign in, sign up, reset password and activate account', 
   userEvent.type(signUpOrcidInput, '0000-0002-1825-0097');
   expect(signUpOrcidInput).toBeValid();
 
-  // Test signup flow
+  // Test signup positive flow
   userEvent.type(signUpNameInput, 'Pep Sanchez');
   userEvent.type(signUpEmailInput, 'pep@ebi.ac.uk');
   userEvent.type(signUpPasswordInput, '123456');
   userEvent.click(signUpTermsCheck);
+  userEvent.click(signUpRecaptcha);
 
   // After terms has been accepted Register button should be enabled
   expect(signUpSubmitButton).toBeEnabled();
 
-  screen.debug();
+  userEvent.click(signUpSubmitButton);
+
+  expect(await screen.findByText('Thank you for registering')).toBeInTheDocument();
+
+  // Test signup negative flow (i.e the email is already registered)
+  userEvent.type(signUpNameInput, 'Pep Sanchez');
+  userEvent.type(signUpEmailInput, 'pep@ebi.ac.uk');
+  userEvent.type(signUpPasswordInput, '123456');
+  userEvent.click(signUpTermsCheck);
+  userEvent.click(signUpRecaptcha);
+
+  server.use(
+    rest.post('/api/auth/register', (_, res, ctx) => {
+      return res(ctx.status(BAD_REQUEST), ctx.json({ log: { message: 'User is already registered' } }));
+    })
+  );
 
   userEvent.click(signUpSubmitButton);
+
+  expect(await screen.findByText('Thank you for registering')).toBeInTheDocument();
+
+  screen.debug();
 
   // TEST SIGNIN
   await navigate('/signin');
