e6df67a304ce3b15cdd33fda031d176d17d76835
Nestor Diaz <ndiaz@ebi.ac.uk>
2022-03-02 11:36:46 +0000

    Remove sudo

--- tasks/build_setup.sh
+++ tasks/build_setup.sh
@@ -14,7 +14,7 @@ set -v
 # Install envsubst
 curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o envsubst
 chmod +x envsubst
-sudo mv envsubst /usr/local/bin
+mv envsubst /usr/local/bin
 
 envsubst < src/config.json > src/config.json
 
