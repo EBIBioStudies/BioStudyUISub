e7b38d568f8563ffdc19468d84a4f338d4748204
Nestor Diaz <ndiaz@ebi.ac.uk>
2022-03-03 10:40:44 +0000

    Try native ensubst

--- tasks/build_setup.sh
+++ tasks/build_setup.sh
@@ -9,46 +9,17 @@ set -v
 # Clean build dist folder
 # rm -rf dist/public && mkdir dist/public
 
-# apt-get update && apt-get install gettext-base
+apt-get update && apt-get install gettext-base
 
 # Install envsubst
-curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o envsubst
-chmod +x envsubst
-mv envsubst /usr/local/bin
+# curl -L https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-`uname -s`-`uname -m` -o envsubst
+# chmod +x envsubst
+# mv envsubst /usr/local/bin
 
 cat src/config.json | envsubst > src/config-copy.json
 mv src/config-copy.json src/config.json
 
-# envsubst < src/config.json
-
 cat src/config.json
-# Config properties
-# contextPath=${CONTEXT_PATH};
-# instanceKey=${INSTANCE_KEY};
-# appPort=${PORT}
-# appProxyBase=${APP_PROXY_BASE};
-# frontendURL=${FRONTEND_URL}
-# backendHostName=${BACKEND_HOST_NAME}
-# backendPathContext=${BACKEND_PATH_CONTEXT}
-# backendPort=${BACKEND_PORT}
-# ciEnvironment=${CI_ENVIRONMENT_SLUG};
-# ciEnvironmentName=${CI_ENVIRONMENT_NAME};
-# mainSuperUserUsername=${APP_SUPER_USER_USERNAME}
-# announcementHeadline=${APP_ANNOUNCEMENT_HEADLINE}
-# announcementContent=${APP_ANNOUNCEMENT_CONTENT}
-# announcementPriority=${APP_ANNOUNCEMENT_PRIORITY}
-
-# Update config.json
-# sed -i 's|"APP_PROXY_BASE":.*|"APP_PROXY_BASE":"'$appProxyBase'",|g' src/config.json
-# sed -i 's|"APP_ENV":.*|"APP_ENV":"'$ciEnvironmentName'",|g' src/config.json
-# sed -i 's|"APP_CONTEXT":.*|"APP_CONTEXT":"'$contextPath'",|g' src/config.json
-# sed -i 's|"FRONTEND_URL":.*|"FRONTEND_URL":"'$frontendURL'",|g' src/config.json
-# sed -i 's|"APP_INSTANCE_KEY".*|"APP_INSTANCE_KEY":"'$instanceKey'",|g' src/config.json
-# sed -i 's|"APP_SUPER_USER_USERNAME".*|"APP_SUPER_USER_USERNAME":"'$mainSuperUserUsername'",|g' src/config.json
-# sed -i 's|"APP_ANNOUNCEMENT_HEADLINE".*|"APP_ANNOUNCEMENT_HEADLINE":""$announcementHeadline"",|g' src/config.json
-# sed -i 's|"APP_ANNOUNCEMENT_CONTENT".*|"APP_ANNOUNCEMENT_CONTENT":""$announcementContent"",|g' src/config.json
-# Last line, don't append a comma
-# sed -i 's|"APP_ANNOUNCEMENT_PRIORITY".*|"APP_ANNOUNCEMENT_PRIORITY":"'$announcementPriority'"|g' src/config.json
 
 # Create .env file
 # echo "
