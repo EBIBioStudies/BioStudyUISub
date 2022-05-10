f570f2d213ac20d2e6fcb7bfb73c525a35094b6f
Nestor Diaz <ndiaz@ebi.ac.uk>
2022-05-10 09:35:50 +0100

    Revert "Revert "Add integration test for submission list""
    This reverts commit 5b7dd742c385be1c48fcf31aa1ba853fa22ef9a3.

---
+++ src/tests/fixtures/submissions.ts
@@ -0,0 +1,22 @@
+export const submissionListResponse = [
+  {
+    accno: 'S-BSST1407',
+    title: 'Testing submission status',
+    version: 1,
+    ctime: '2022-03-29T13:21:13.557Z',
+    mtime: '2022-03-29T13:21:13.557Z',
+    rtime: '2022-03-31T00:00:00Z',
+    method: 'PAGE_TAB',
+    status: 'PROCESSED'
+  },
+  {
+    accno: 'S-BIAD33',
+    title: 'Testing submission in dev for keywords',
+    version: 2,
+    ctime: '2022-03-14T10:21:21.574Z',
+    mtime: '2022-03-14T10:33:14.8Z',
+    rtime: '2022-03-14T00:00:00Z',
+    method: 'PAGE_TAB',
+    status: 'PROCESSED'
+  }
+];
