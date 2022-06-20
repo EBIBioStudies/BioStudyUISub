5f3be155a79014c2b81d0cee0f29cc50b903a4fb
Awais Athar <awais@ebi.ac.uk>
2022-06-20 14:26:39 +0100

    [Delivers #182284353] Fix contact name on new draft

--- src/app/submission/submission-edit/shared/subm-edit.service.ts
+++ src/app/submission/submission-edit/shared/subm-edit.service.ts
@@ -271,7 +271,7 @@ export class SubmEditService {
 
   private asContactAttributes(userInfo: UserInfo): AttributeData[] {
     return [
-      { name: 'Name', value: userInfo.username || '' },
+      { name: 'Name', value: userInfo.fullname || userInfo.username || '' },
       { name: 'E-mail', value: userInfo.email },
       { name: 'ORCID', value: userInfo.aux.orcid }
     ];
