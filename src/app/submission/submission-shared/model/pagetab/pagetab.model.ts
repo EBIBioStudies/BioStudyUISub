af4c228a97cc801fd69479adf79453f8b9ae108d
Liviu Anita <liviu@ebi.ac.uk>
2022-11-14 10:41:33 +0000

    Fix failing test

--- src/app/submission/submission-shared/model/pagetab/pagetab.model.ts
+++ src/app/submission/submission-shared/model/pagetab/pagetab.model.ts
@@ -23,7 +23,7 @@ export class AttrExceptions {
     { name: 'AccNoPattern', rootLevel: true, studyLevel: false, systemOnly: true, unique: true },
     { name: 'AccNoTemplate', rootLevel: true, studyLevel: false, systemOnly: true, unique: true },
     { name: 'Template', rootLevel: true, studyLevel: false, systemOnly: true, unique: true },
-    { name: 'Title', rootLevel: true, studyLevel: true, systemOnly: false, unique: false }
+    { name: 'Title', rootLevel: true, studyLevel: true, systemOnly: false, unique: true }
   ];
 
   private static editableAttr: Array<string> = AttrExceptions.allAttrs
