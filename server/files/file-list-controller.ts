667b65e782b0621dfc7913f924ddcaad52ec3d21
Awais Athar <awais@ebi.ac.uk>
2023-07-17 15:16:00 +0100

    Fix proxy crash on missing cookie

--- server/files/file-list-controller.ts
+++ server/files/file-list-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import needle from 'needle';
 import { PathInfo } from '../../src/app/file/shared/file-rest.model';
+
 export const fileListController = (path: string, router: Router) => {
   router.get(path, async (req: Request, res: Response) => {
     const folder = req.url.substr('/file-list/'.length);
@@ -8,30 +9,37 @@ export const fileListController = (path: string, router: Router) => {
       'Content-Disposition': `attachment; filename= ${folder.startsWith('user/') ? folder.substr(5) : folder}.tsv`,
       'Content-Type': 'text/tab-separated-values'
     });
-    res.write('Files\n');
     processFolder(folder, req, res).then(() => res.send());
   });
 
   function processFolder(folder: string, req: Request, res: Response) {
     const url = `${req.get('host')}/api/files/user/${folder}`;
-    const headers = { 'X-Session-Token': req.cookies['BioStudiesToken'] };
+    const headers = { 'X-Session-Token': req?.cookies['BioStudiesToken'] || '' };
 
     return new Promise((resolve) => {
-      needle('get', url, { headers: headers }).then((response) => {
-        const folders: string[] = [];
-        if (response.statusCode != 200) {
+      needle('get', url, { headers: headers })
+        .then((response) => {
+          const folders: string[] = [];
+          if (response.statusCode != 200) {
+            res.sendStatus(response.status);
+            resolve();
+            return;
+          }
+          res.write('Files\n');
+          response.body.forEach((node: PathInfo) => {
+            if (node.type === 'FILE') {
+              res.write(`${node.path.startsWith('user/') ? node.path.substr(5) : node.path}/${node.name}\n`);
+            } else {
+              folders.push(`${node.path}/${node.name}`);
+            }
+          });
+          Promise.all(folders.sort().map((folder) => processFolder(folder, req, res))).then(() => resolve());
+        })
+        .catch(function (err) {
+          res.sendStatus(err?.status || 500);
           resolve();
           return;
-        }
-        response.body.forEach((node: PathInfo) => {
-          if (node.type === 'FILE') {
-            res.write(`${node.path.startsWith('user/') ? node.path.substr(5) : node.path}/${node.name}\n`);
-          } else {
-            folders.push(`${node.path}/${node.name}`);
-          }
         });
-        Promise.all(folders.sort().map((folder) => processFolder(folder, req, res))).then(() => resolve());
-      });
     });
   }
 };
